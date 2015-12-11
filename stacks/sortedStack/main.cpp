fec263a3089a75d1308c76b58edb78d658762611
Christopher Zhang <christopherzhang3@gmail.com>
2015-12-11 14:40:27 -0500

    Sort a stack using only one extra stack as temporary storage.

---
+++ stacks/sortedStack/main.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <assert.h>
+#include <cstdlib>
+#include <ctime>
+
+#define MAX_STACK_LENGTH 15
+#define NUM_ITERATIONS 10000
+
+// Sort a stack in decreasing (biggest on top) order
+
+struct Node {
+    int val;
+    Node * next;
+    Node(int val) {
+        this->val = val;
+        next = nullptr;
+    }
+};
+
+struct Stack {
+    Node * top;
+    Stack() {
+        top = nullptr;
+    }
+    ~Stack() {
+        Node * nptr = top;
+        while (nptr) {
+            Node * temp = nptr->next;
+            delete nptr;
+            nptr = temp;
+        }
+    }
+    void push(Node * n) {
+        if (!n) { return; }
+        if (!top) {
+            top = n;
+        } else {
+            n->next = top;
+            top = n;
+        }
+    }
+    Node * pop() {
+        if (!top) { return nullptr; }
+        Node * temp = top;
+        top = top->next;
+        temp->next = nullptr;
+        return temp;
+    }
+    bool isEmpty() {
+        return (top == nullptr);
+    }
+    void pushSorted(Node * n, Stack & s) {
+        // Use s as a buffer to help maintain sorting of current stack
+        Node * oldTop = s.top;
+        if ((!top) || (n->val <= top->val)) {
+            push(n);
+            return;
+        }
+        while(top && n->val > top->val) {
+            s.push(pop());
+        }
+        push(n);
+        while (s.top != oldTop) {
+            push(s.pop());
+        }
+    }
+    bool isSorted() {
+        if (top == nullptr) { return true; }
+        Node * nptr = top;
+        while (nptr) {
+            if (nptr->next) {
+                if (nptr->val < nptr->next->val) { return false; }
+            }
+            nptr = nptr->next;
+        }
+        return true;
+    }
+};
+
+std::ostream & operator<<(std::ostream & os, const Stack & s) {
+    Node * nptr = s.top;
+    os << "top <- ";
+    while (nptr) {
+        os << nptr->val << " ";
+        nptr = nptr->next;
+    }
+    return os;
+}
+
+int main() {
+    srand(time(0));
+    for (int iteration = 0; iteration < NUM_ITERATIONS; ++iteration) {
+        int stack_length = rand() % MAX_STACK_LENGTH + 1;
+        Stack s;
+        Stack aux; // Allowed 1 extra stack only to help sort
+        // Fill the stack with random values
+        for (int nodeIndex = 0; nodeIndex < stack_length; ++nodeIndex) {
+            int randval = rand() % 100;
+            s.push(new Node(randval));
+        }
+        std::cout << s ;
+        std::cout << "-> sorted -> ";
+        // Begin sorting
+        while (!s.isEmpty()) {
+            aux.pushSorted(s.pop(), s);
+        }
+        while (!aux.isEmpty()) {
+            s.push(aux.pop());
+        }
+        std::cout << s << std::endl;
+        assert(s.isSorted());
+    }
+    return 0;
+}
