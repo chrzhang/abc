6661557a70ad0f1868b3ecb038f4301434685946
Christopher Zhang <christopherzhang3@gmail.com>
2015-12-11 02:09:34 +0000

    Allow popAt to pop a node from a specific substack while maintaining sizes of original filled substacks (pointer manipulation to simulate shift).

--- stacks/setOfStacks/main.cpp
+++ stacks/setOfStacks/main.cpp
@@ -15,9 +15,10 @@ struct Node {
     intptr_t val;
     bool isSentinel;
     Node * next;
+    Node * prev;
     Node(intptr_t val, bool isSentinel = false) {
         this->val = val;
-        next = nullptr;
+        next = prev = nullptr;
         this->isSentinel = isSentinel;
     }
 };
@@ -46,6 +47,7 @@ struct SetOfStacks {
             // Build and insert a sentinel
             Node * sentinel = new Node(-1, true);
             sentinel->next = top;
+            top->prev = sentinel;
             // Store the sentinel's address in listOfSentinelAddresses
             assert(sentinelAddressesMap.find(currentSubstack)
                    == sentinelAddressesMap.end());
@@ -57,7 +59,9 @@ struct SetOfStacks {
         }
         // Insert the node n
         n->next = top;
+        if (top) { top->prev = n; }
         top = n;
+        assert(top->prev == nullptr);
         // Increment the offset in the substack
         ++currentSubstackOffset;
     }
@@ -66,12 +70,14 @@ struct SetOfStacks {
         assert(!top->isSentinel);
         Node * temp1 = top;
         top = top->next;
+        if (top) { top->prev = nullptr; }
         if (top && top->isSentinel) {
             --currentSubstack;
             sentinelAddressesMap.erase(currentSubstack);
             Node * temp2 = top->next;
             delete top;
             top = temp2;
+            top->prev = nullptr;
         }
         if (currentSubstackOffset == 1 && top) {
             currentSubstackOffset = SUB_STACK_MAX_HEIGHT;
@@ -80,25 +86,63 @@ struct SetOfStacks {
         }
         return temp1;
     }
-    Node * popAt(int subStackIndex) {
-        if (subStackIndex == currentSubstack + 1) {
+    // Pop from a specific substack and "shift" everything over with ptr manip
+    Node * popAt(size_t subStackIndex) {
+        if (subStackIndex == currentSubstack) {
             return pop();
+        } else if (subStackIndex > currentSubstack) {
+            // Asking for a substack that does not exist
+            std::cout << "substack with index " << subStackIndex
+                      << " does not exist." << std::endl;
+            return nullptr;
         }
         assert(sentinelAddressesMap.find(subStackIndex) !=
                sentinelAddressesMap.end());
+        Node * sentinel = (Node *) sentinelAddressesMap[subStackIndex];
+        assert(sentinel);
+        std::cout << "current substack: " << currentSubstack << std::endl;
+        // assert(sentinel->prev->prev);
         // Remove the node after the sentinel for the substack
-        // Remove the node before the sentinel and put it as next
+        Node * n = sentinel->next;
+        assert(n);
+        sentinel->next = sentinel->next->next;
+        sentinel->next->prev = sentinel;
         // Swap all sentinels afterwards with their predecessors
-        return nullptr;
+        for (int i = subStackIndex; sentinelAddressesMap.find(i) !=
+                                    sentinelAddressesMap.end(); ++i) {
+            Node * sentinel = (Node *) sentinelAddressesMap[i];
+            assert(sentinel);
+            Node * pn = sentinel->prev;
+            Node * sn = sentinel->next;
+            if (!pn->prev) {
+                assert(top == pn);
+                pn->next = sn;
+                sn->prev = pn;
+                delete sentinel;
+                sentinelAddressesMap.erase(i);
+                --currentSubstack;
+                break;
+            }
+            pn->prev->next = sentinel;
+            sentinel->prev = pn->prev;
+            sentinel->next = pn;
+            pn->prev = sentinel;
+            pn->next = sn;
+            sn->prev = pn;
+        }
+        --currentSubstackOffset;
+        n->prev = n->next = nullptr; // Cut it off before returning
+        return n;
     }
 };
 
 std::ostream & operator<<(std::ostream & os, const SetOfStacks & sos) {
+    int ct = sos.currentSubstack - 1;
     os << "top <- ";
     Node * nptr = sos.top;
     while (nptr) {
         if (nptr->isSentinel) {
-            os << "s ";
+            os << "[stack " << ct-- << "] ";
         } else {
             os << nptr->val << " ";
         }
@@ -120,7 +164,8 @@ bool matches(const std::deque<int> & d, const SetOfStacks & sos) {
 
 void printSentinels(const std::unordered_map<int, intptr_t> & map) {
     for (auto it = map.begin(); it != map.end(); ++it) {
-        // TODO Print information about predecessor and successor for debugging
+        std::cout << ((Node *) (it->second))->prev->val << " s" << it->first
+                  << " " << ((Node *) (it->second))->next->val << std::endl;
     }
 }
 
@@ -136,8 +181,8 @@ int main() {
                     sos.push(new Node(randval));
                     standardStack.push_front(randval);
                     assert(matches(standardStack, sos));
-                    // std::cout << "+ Pushed " << sos;
-                    printSentinels(sos.sentinelAddressesMap);
+                    //std::cout << "+ Pushed " << sos;
+                    //printSentinels(sos.sentinelAddressesMap);
                     break;
                 }
                 case 1: {
@@ -147,12 +192,38 @@ int main() {
                         standardStack.pop_front();
                     }
                     assert(matches(standardStack, sos));
-                    // std::cout << "- Popped " << sos;
-                    printSentinels(sos.sentinelAddressesMap);
+                    //std::cout << "- Popped " << sos;
+                    //printSentinels(sos.sentinelAddressesMap);
                     break;
                 }
             }
         }
+    }
+    std::cout << "Testing popAt(stackIndex)\n";
+    // Tested with a size of 14 and trying 0 repeatedly until past empty
+    // Tested with a size of 14 and trying 1 repeatedly until only stack 0 left
+    // Tested with a size of 5 and trying 1,2... (for stacks that DNE)
+    SetOfStacks sos;
+    std::string input;
+    int length = 0;
+    while (length <= 0) {
+        std::cout << "Enter a pos. length for the total set of stacks: ";
+        getline(std::cin, input);
+        length = stoi(input);
+    }
+    for (int i = 0; i < stoi(input); ++i) {
+        sos.push(new Node(rand() % 100));
+    }
+    // Each stack is separated by s, to denote a sentinel
+    std::cout << "Generated a new set of stacks:\n" << sos;
+    input.clear();
+    int subStackIndex = 1;
+    while (subStackIndex >= 0) {
+        std::cout << "Enter a substack index to popAt (-1 to quit): ";
+        getline(std::cin, input);
+        subStackIndex = stoi(input);
+        delete sos.popAt(subStackIndex);
+        std::cout << sos;
     }
     return 0;
 }
