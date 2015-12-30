8ca681747fa826ac50c2671ecf91a9fd724a76e2
Christopher Zhang <christopherzhang3@gmail.com>
2015-12-30 13:03:47 -0500

    Store past calculations of finding ways to reach destination.

--- recursion/waysToCrossGridLeftDown/main.cpp
+++ recursion/waysToCrossGridLeftDown/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <assert.h>
+#include <iomanip>
 
-#define X 3
-#define Y 3
+#define X 10
+#define Y 10
 
 // Count ways from top left of a X + 1 by Y + 1 grid (0,0) to reach (X,Y)
 
@@ -23,9 +24,33 @@ size_t waysToReach(const Position & src, const Position & dest) {
            waysToReach(Position(src.x, src.y + 1), dest);
 }
 
+size_t waysToReachDP(size_t (&table)[X + 1][Y + 1], const Position & src,
+                     const Position & dest) {
+    if (src.x == dest.x || src.y == dest.y) {
+        table[src.x][src.y] = 1;
+        return 1;
+    }
+    if (table[src.x][src.y]) { return table[src.x][src.y]; }
+    size_t sum = waysToReachDP(table, Position(src.x + 1, src.y), dest) +
+                 waysToReachDP(table, Position(src.x, src.y + 1), dest);
+    table[src.x][src.y] = sum;
+    return sum;
+}
+
 int main() {
-    std::cout << waysToReach(Position(0,0), Position(X,Y)) << std::endl;
-    assert(20 == waysToReach(Position(0,0), Position(3,3)));
-    assert(10 == waysToReach(Position(0,0), Position(3,2)));
+    size_t table[X + 1][Y + 1]; // Store previous calculations
+    for (int row = 0; row < X + 1; ++row) {
+        for (int col = 0; col < Y + 1; ++col) {
+            table[row][col] = 0;
+        }
+    }
+    assert(waysToReachDP(table, Position(0,0), Position(X,Y)) ==
+           waysToReach(Position(0,0), Position(X,Y)));
+    for (int row = 0; row < X + 1; ++row) {
+        for (int col = 0; col < Y + 1; ++col) {
+            std::cout << std::setw(8) << table[row][col];
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
