56f1bd4238ed8dc5d60f1d806c1ea703d37a5d10
Christopher Zhang <christopherzhang3@gmail.com>
2015-12-30 01:05:44 -0500

    Use dynamic programming to remember how many ways there are to hop any number of steps.

--- recursion/waysToClimbSteps/main.cpp
+++ recursion/waysToClimbSteps/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <assert.h>
 
 // Find how many ways are there to climb N steps (1,2,3 step increments)
 
@@ -19,10 +21,35 @@ size_t waysToClimb(size_t numSteps) {
     return acc;
 }
 
+size_t waysToClimbDP(std::unordered_map<size_t, size_t> & table,
+                   size_t numSteps) {
+    if (table.find(numSteps) != table.end()) {
+        return table[numSteps];
+    }
+    if (0 == numSteps) {
+        table[0] = 1;
+        return 1;
+    }
+    size_t acc = 0;
+    if (numSteps >= 1) {
+        acc += waysToClimbDP(table, numSteps - 1);
+    }
+    if (numSteps >= 2) {
+        acc += waysToClimbDP(table, numSteps - 2);
+    }
+    if (numSteps >= 3) {
+        acc += waysToClimbDP(table, numSteps - 3);
+    }
+    table[numSteps] = acc;
+    return acc;
+}
+
 int main() {
+    std::unordered_map<size_t, size_t> table;
     for (int i = 0; true; ++i) {
         std::cout << "Ways to climb " << i << " steps: " << waysToClimb(i)
                   << std::endl;
+        assert(waysToClimb(i) == waysToClimbDP(table, i));
     }
     return 0;
 }
