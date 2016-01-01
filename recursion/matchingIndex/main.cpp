89b3080bb9dbf156bba8134c354e192aca020815
Christopher Zhang <christopherzhang3@gmail.com>
2016-01-01 22:04:41 +0000

    Find a matching index in a non-distinct, sorted array.

--- recursion/matchingIndex/main.cpp
+++ recursion/matchingIndex/main.cpp
@@ -9,6 +9,7 @@
 // Find values that match their indices in a sorted set of numbers
 
 int findMagicIndex(int arr[ARR_SIZE], int begin, int end) {
+    if (begin > end) { return -1; }
     int mid = (begin + end) / 2;
     if (arr[mid] == mid) {
         return mid;
@@ -19,20 +20,51 @@ int findMagicIndex(int arr[ARR_SIZE], int begin, int end) {
     }
 }
 
+int min(int a, int b) { return a < b ? a : b; }
+int max(int a, int b) { return a > b ? a : b; }
+
+int findMagicIndexNotDistinct(int arr[ARR_SIZE], int begin, int end) {
+    if (begin > end) { return -1; }
+    int mid = (begin + end) / 2;
+    if (arr[mid] == mid) {
+        return mid;
+    }
+    // If the middle index 4 has value 1, there is no way index 3 will match
+    int leftRes = findMagicIndexNotDistinct(arr, begin, min(mid - 1, arr[mid]));
+    if (leftRes != -1) { return leftRes; }
+    // If the middle index 4 has value 7, there is no way index 5 will match
+    return findMagicIndexNotDistinct(arr, max(mid + 1, arr[mid]), end);
+}
+
 int main() {
     srand(time(0));
     for (int iteration = 0; iteration < NUM_ITERATIONS; ++iteration) {
-        int arr[ARR_SIZE];
-        int magicIndex = rand() % ARR_SIZE;
-        arr[magicIndex] = magicIndex;
-        for (int i = magicIndex - 1; i >= 0; --i) {
-            arr[i] = arr[i + 1] - (rand() % 5 + 1);
+        { // With distinct elements
+            int arr[ARR_SIZE];
+            int magicIndex = rand() % ARR_SIZE;
+            arr[magicIndex] = magicIndex;
+            for (int i = magicIndex - 1; i >= 0; --i) {
+                arr[i] = arr[i + 1] - (rand() % 5 + 1);
+            }
+            for (int i = magicIndex + 1; i < ARR_SIZE; ++i) {
+                arr[i] = arr[i - 1] + (rand() % 5 + 1);
+            }
+            int mi = findMagicIndex(arr, 0, ARR_SIZE - 1);
+            assert(arr[mi] == mi);
         }
-        for (int i = magicIndex + 1; i < ARR_SIZE; ++i) {
-            arr[i] = arr[i - 1] + (rand() % 5 + 1);
+        { // With possibly repeating elements
+            int arr[ARR_SIZE];
+            int magicIndex = rand() % ARR_SIZE;
+            arr[magicIndex] = magicIndex;
+            for (int i = magicIndex - 1; i >= 0; --i) {
+                arr[i] = arr[i + 1] - (rand() % 5);
+            }
+            for (int i = magicIndex + 1; i < ARR_SIZE; ++i) {
+                arr[i] = arr[i - 1] + (rand() % 5);
+            }
+            int mi = findMagicIndexNotDistinct(arr, 0, ARR_SIZE - 1);
+            assert(arr[mi] == mi);
         }
-        int mi = findMagicIndex(arr, 0, ARR_SIZE - 1);
-        assert(arr[mi] == mi);
     }
     return 0;
 }
