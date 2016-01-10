1e5c0945c3cb86316ab55ca98c745202c03f167f
Christopher Zhang <christopherzhang3@gmail.com>
2016-01-10 18:27:38 +0000

    Fix comments and add test cases for in-place swap.

--- swaps/inPlaceSwap/main.cpp
+++ swaps/inPlaceSwap/main.cpp
@@ -3,6 +3,10 @@
 #include <cstdlib>
 #include <assert.h>
 
+#define NUM_ITERATIONS 1000000
+
+// Swap two numbers in-place (do not use any temporary variables)
+
 void swap(int & i, int & j) { // Beware of overflow when adding
     i += j;
     j = i - j;
@@ -10,10 +14,20 @@ void swap(int & i, int & j) { // Beware of overflow when adding
 }
 
 int main() {
+    srand(time(0));
     int i, j;
     i = 1; j = 2;
     swap(i, j);
     assert(i == 2);
     assert(j == 1);
+    for (int iteration = 0; iteration < NUM_ITERATIONS; ++iteration) {
+        i = rand() % 1000;
+        j = rand() % 1000;
+        auto oldI = i;
+        auto oldJ = j;
+        swap(i, j);
+        assert(j == oldI);
+        assert(i == oldJ);
+    }
     return 0;
 }
