eb6a474fcda77ba40a96355d21557c6991b42a2f
Christopher Zhang <christopherzhang3@gmail.com>
2016-01-11 14:03:25 -0500

    Change algorithm and test up to 1000! using GMP.

--- factorials/countTrailing0s/main.cpp
+++ factorials/countTrailing0s/main.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 #include <assert.h>
+#include <gmpxx.h>
 
-#define N 12 // Any more won't be valid because of overflow
+#define N 1000 // Use GMP library with C++ interface for arbitrary precision
 
 // Count trailing zeroes in N!
 
-unsigned fact(unsigned n) {
+mpz_class fact(mpz_class n) {
     if (n == 0) { return 1; }
     return n * fact(n - 1);
 }
 
-unsigned countTrailingZeroesIn(unsigned n) {
+mpz_class countTrailingZeroesIn(mpz_class n) {
     if (n == 0) { return 1; }
-    unsigned counter = 0;
-    while (!(n % 10)) {
+    mpz_class counter = 0;
+    while (n % 10 == 0) {
         ++counter;
         n /= 10;
     }
     return counter;
 }
 
-unsigned countTrailingZeroesInFactorialOf(unsigned n) {
-    if (N == 0) { return 0; }
-    unsigned currLastDigit = 1;
-    unsigned numTrailingZeroes = 0;
-    for (unsigned i = 1; i <= n; ++i) {
-        auto j = i;
-        auto tz = countTrailingZeroesIn(j);
-        numTrailingZeroes += tz;
-        if (tz) {
-            j /= 10 * tz;
-        }
-        if (currLastDigit) { // Multiplying by 0 will not introduce new 0s
-            currLastDigit *= j % 10;
-            currLastDigit %= 10;
-            numTrailingZeroes += currLastDigit == 0 ? 1 : 0;
-        }
+mpz_class min(mpz_class i, mpz_class j) {
+    return i > j ? j : i;
+}
+
+mpz_class countOccIn(mpz_class factor, mpz_class n) {
+    mpz_class ct = 0;
+    while (n % factor == 0) {
+        n /= factor;
+        ++ct;
     }
-    return numTrailingZeroes;
+    return ct;
+}
+
+mpz_class countTrailingZeroesInFactorialOf(mpz_class n) {
+    if (n == 0) { return 0; }
+    // A trailing zero is introduced when the multiplication involves a 10
+    // formed by multiplying 5 by 2 (thus we count the # of 5s and 2s)
+    mpz_class numberOf2s = 0;
+    mpz_class numberOf5s = 0;
+    for (int i = 1; i <= n; ++i) {
+        numberOf2s += countOccIn(2, i);
+        numberOf5s += countOccIn(5, i);
+    }
+    return min(numberOf5s, numberOf2s);
 }
 
 int main() {
-    for (unsigned i = 0; i <= N; ++i) {
+    for (mpz_class i = 0; i <= N; ++i) {
         assert(countTrailingZeroesIn(fact(i)) ==
                countTrailingZeroesInFactorialOf(i));
     }
