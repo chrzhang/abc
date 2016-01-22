10814fbca05bb9b1e426439a60a8e4ee46f78e72
Christopher Zhang <christopherzhang3@gmail.com>
2016-01-21 19:45:01 -0500

    Store optimal sentences for substrings to avoid recalculation.

--- langs/sensicalSentences/main.cpp
+++ langs/sensicalSentences/main.cpp
@@ -2,17 +2,25 @@
 #include <string>
 #include <set>
 #include <cassert>
+#include <map>
 
 // Given a sentence without whitespace, find a way to split the character so
 // that the fewest numbers of characters are left as unknown words
 
 int findSplit(const std::set<std::string> dict, const std::string & s,
-              int begin, int current, std::set<int> & indices) {
+              int begin, int current, std::set<int> & indices,
+              std::map<std::pair<int, int>, int> & results) {
+    auto seekOldResults = results.find(std::pair<int, int>(begin, current));
+    if (seekOldResults != results.end()) {
+        return seekOldResults->second;
+    }
     // Base case
     if (current + 1 == s.size()) {
         if (dict.find(s.substr(begin, current - begin + 1)) != dict.end()) {
+            results[std::pair<int, int>(begin, current)] = 0;
             return 0;
         } else {
+            results[std::pair<int, int>(begin, current)] = current - begin + 1;
             return (current - begin + 1);
         }
     }
@@ -22,11 +30,15 @@ int findSplit(const std::set<std::string> dict, const std::string & s,
         numPrefixInvalidChars += current - begin + 1;
     }
     int lenIfSplit = numPrefixInvalidChars + findSplit(dict, s, current + 1,
-                                                       current + 1, indices);
-    int lenIfNotSplit = findSplit(dict, s, begin, current + 1, indices);
+                                                       current + 1, indices,
+                                                       results);
+    int lenIfNotSplit = findSplit(dict, s, begin, current + 1, indices,
+                                  results);
     if (current && lenIfSplit < lenIfNotSplit) {
         indices.insert(current);
     }
+    results[std::pair<int, int>(begin, current)] = lenIfSplit < lenIfNotSplit ?
+                                         lenIfSplit : lenIfNotSplit;
     return (lenIfSplit < lenIfNotSplit ? lenIfSplit : lenIfNotSplit);
 }
 
@@ -35,7 +47,8 @@ std::string sensicalSplit(const std::set<std::string> dict,
                           const std::string & s) {
     std::string str = s;
     std::set<int> indices; // Store indices where putting spaces yields solution
-    findSplit(dict, s, 0, 0, indices);
+    std::map<std::pair<int, int>, int> results; // Store old results (DP)
+    findSplit(dict, s, 0, 0, indices, results);
     int offset = 0;
     for (auto index = indices.begin(); index != indices.end(); ++index) {
         str.insert(*index + 1 + offset, 1, ' ');
