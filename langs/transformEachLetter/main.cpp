49ee57944cdda4cfd26833e8f15aece89fbf3b53
Christopher Zhang <christopherzhang3@gmail.com>
2016-01-30 20:27:09 +0000

    Halt when target word is generated.

--- langs/transformEachLetter/main.cpp
+++ langs/transformEachLetter/main.cpp
@@ -15,7 +15,7 @@ int isOneEditAway(const std::string & s1, const std::string & s2) {
     auto it1 = s1.begin();
     auto it2 = s2.begin();
     int foundDifferenceAt = -1;
-    for (int i = 0; i < s1.size(); ++i) {
+    for (int i = 0; i < (int) s1.size(); ++i) {
         assert(it1 != s1.end());
         assert(it2 != s2.end());
         if (*it1 != *it2) {
@@ -37,34 +37,44 @@ void testIsOneEditAway() {
     assert(-1 == isOneEditAway("hello", "hello"));
 }
 
-void findTransformations(const std::string & s1, const std::string & s2,
+bool findTransformations(const std::string & s1, const std::string & s2,
                          const std::set<std::string> & allWords,
                          std::vector<bool> & visited) {
     //std::cout << "s1: " << s1 << " s2: " << s2 << std::endl;
-    std::cout << s1 << std::endl;
+    std::cout << s1 << "\n";
     auto seek1 = allWords.find(s1);
     auto seek2 = allWords.find(s2);
     assert(seek1 != allWords.end() && seek2 != allWords.end());
-    if (s1.compare(s2) == 0) { return; }
+    if (s1.compare(s2) == 0) { return true; }
     visited[std::distance(allWords.begin(), seek1)] = true;
     for (auto it = allWords.begin(); it != allWords.end(); ++it) {
         auto word = *it;
         if (visited[std::distance(allWords.begin(), it)]) { continue; }
         auto indexOfDifference = isOneEditAway(word, s1);
         if (-1 != indexOfDifference) {
-            findTransformations(word, s2, allWords, visited);
+            if (findTransformations(word, s2, allWords, visited))  {
+                return true;
+            }
         }
     }
+    return false;
 }
 
 int main() {
    testIsOneEditAway();
    std::set<std::string> allWords;
-   allWords.insert("foo");
-   allWords.insert("boo");
-   allWords.insert("bor");
-   allWords.insert("bar");
-   allWords.insert("baz");
+   // Add all possible 3 letter words
+   for (int i = 0; i < 26; ++i) {
+       for (int j = 0; j < 26; ++j) {
+           for (int k = 0; k < 26; ++k) {
+               std::string word;
+               word += 'a' + i;
+               word += 'a' + j;
+               word += 'a' + k;
+               allWords.insert(word);
+           }
+       }
+   }
    std::vector<bool> visited(allWords.size(), false);
    findTransformations("foo", "baz", allWords, visited);
    return 0;
