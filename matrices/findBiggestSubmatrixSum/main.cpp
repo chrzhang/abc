ee2cf3326a6abd2016448d4a869f4b437c49942c
Christopher Zhang <christopherzhang3@gmail.com>
2016-02-02 17:05:41 +0000

    Improve algorithm by storing past calculations of submatrix sums.

--- matrices/findBiggestSubmatrixSum/main.cpp
+++ matrices/findBiggestSubmatrixSum/main.cpp
@@ -29,7 +29,35 @@ int sumOfSubmatrix(int nwRow, int nwCol, int seRow, int seCol, int m[N][N]) {
     return sum;
 }
 
-void bfFindSubmatrixWithBiggestSum(int m[N][N]) { // Brute force
+void findPrecomputedSums(int nwRow, int nwCol, int precomputedSums[N][N],
+                         int m[N][N]) {
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < N; ++col) {
+            precomputedSums[row][col] = 0;
+        }
+    }
+    for (int row = nwRow; row < N; ++row) {
+        for (int col = nwCol; col < N; ++col) {
+            if (row == nwRow && col == nwCol) {
+                precomputedSums[row][col] = m[row][col];
+            } else if (row == nwRow) {
+                precomputedSums[row][col] = precomputedSums[row][col - 1] +
+                                            m[row][col];
+            } else if (col == nwCol) {
+                precomputedSums[row][col] = precomputedSums[row - 1][col] +
+                                            m[row][col];
+            } else {
+                int sum = precomputedSums[row - 1][col];
+                for (int scol = nwCol; scol <= col; ++scol) {
+                    sum += m[row][scol];
+                }
+                precomputedSums[row][col] = sum;
+            }
+        }
+    }
+}
+
+void findSubmatrixWithBiggestSum(int m[N][N]) {
     auto currentMax = INT_MIN;
     auto topLeftRow = -1;
     auto topLeftCol = -1;
@@ -37,11 +65,14 @@ void bfFindSubmatrixWithBiggestSum(int m[N][N]) { // Brute force
     auto botRightCol = -1;
     for (int nwRow = 0; nwRow < N; ++nwRow) {
         for (int nwCol = 0; nwCol < N; ++nwCol) {
+            // Pre-compute sums for every submatrix from the top left
+            int precomputedSums[N][N];
+            findPrecomputedSums(nwRow, nwCol, precomputedSums, m);
             for (int seRow = nwRow; seRow < N; ++seRow) {
                 for (int seCol = nwCol; seCol < N; ++seCol) {
-                    //std::cout << "Top left at " << nwRow << ", " << nwCol << std::endl;
-                    //std::cout << "Bot right at " << seRow << ", " << seCol << std::endl;
-                    auto s = sumOfSubmatrix(nwRow, nwCol, seRow, seCol, m);
+                    //auto s = sumOfSubmatrix(nwRow, nwCol, seRow, seCol, m);
+                    //assert(s == precomputedSums[seRow][seCol]);
+                    auto s = precomputedSums[seRow][seCol];
                     if (s > currentMax) {
                         currentMax = s;
                         topLeftRow = nwRow;
@@ -64,12 +95,10 @@ int main() {
     int m[N][N];
     for (int row = 0; row < N; ++row) {
         for (int col = 0; col < N; ++col) {
-            m[row][col] = (rand() % 101) - 50;
+            m[row][col] = (rand() % 5) - 2;
         }
     }
     printMatrix(m);
-    bfFindSubmatrixWithBiggestSum(m);
+    findSubmatrixWithBiggestSum(m);
     return 0;
 }
-
-
