d49797c9cba5ebb229ea3ad6a1fe991e3e1c80ca
Christopher Zhang <christopherzhang3@gmail.com>
2016-03-15 15:37:51 -0400

    Recursively entertain all possibilities in the constrained search space to solve every puzzle.

--- games/sudokuSolver/main.cpp
+++ games/sudokuSolver/main.cpp
@@ -16,7 +16,7 @@ struct Candidates {
     int setFalse(int i) {
         assert(i >= 1 && i <= 9);
         if (possible[i - 1]) {
-            if (numPossible == 1) { assert(false); }
+            if (numPossible == 1) { return -1; }
             possible[i - 1] = false;
             --numPossible;
             if (1 == numPossible) {
@@ -55,29 +55,40 @@ void printNumPossibilities(Candidates grid[9][9]) {
     }
 }
 
+// Given a square's row or column index, find the index of the NW corner of the
+// 3 x 3 box it lives in
 int getAnchor(int x) {
     return 3 * (x / 3);
 }
 
-void process(int row, int col, char grid[9][9], Candidates gridC[9][9],
+bool isSudokuDigit(char c) {
+    return c >= '1' && c <= '9';
+}
+
+// Return -1 on error (a square no longer has possibilities)
+int process(int row, int col, char grid[9][9], Candidates gridC[9][9],
              std::list<std::pair<int, int>> & pointsOfInterest) {
-    if (!isdigit(grid[row][col])) { return; }
+    if (!isSudokuDigit(grid[row][col])) { return 0; }
     auto c = grid[row][col] - '1' + 1;
     // Remove possibility from the row, col, and current box
     for (int i = 0; i < 9; ++i) {
         if (i == col) { continue; }
         auto r = gridC[row][i].setFalse(c);
-        if (r) {
+        if (r > 0 && !isSudokuDigit(grid[row][i])) {
             grid[row][i] = (r - 1) + '1';
             pointsOfInterest.push_back(std::pair<int, int>(row, i));
+        } else if (r == -1) {
+            return -1;
         }
     }
     for (int i = 0; i < 9; ++i) {
         if (i == row) { continue; }
         auto r = gridC[i][col].setFalse(c);
-        if (r) {
+        if (r > 0 && !isSudokuDigit(grid[i][col])) {
             grid[i][col] = (r - 1) + '1';
             pointsOfInterest.push_back(std::pair<int, int>(i, col));
+        } else if (r == -1) {
+            return -1;
         }
     }
     int m = getAnchor(row);
@@ -86,15 +97,74 @@ void process(int row, int col, char grid[9][9], Candidates gridC[9][9],
         for (int l = 0; l < 3; ++l) {
             if (m + k == row && n + l == col) { continue; }
             auto r = gridC[m + k][n + l].setFalse(c);
-            if (r) {
+            if (r > 0 && !isSudokuDigit(grid[m + k][n + l])) {
                 grid[m + k][n + l] = (r - 1) + '1';
                 pointsOfInterest.push_back(std::pair<int, int>(m + k, n + l));
+            } else if (r == -1) {
+                return -1;
             }
+        }
+    }
+    return 0;
+}
+
+void commit(char grid[9][9], char finalGrid[9][9]) {
+    for (int r = 0; r < 9; ++r) {
+        for (int c = 0; c < 9; ++c) {
+            finalGrid[r][c] = grid[r][c];
         }
     }
 }
 
+int trySolve(char grid[9][9], Candidates gridC[9][9], char finalGrid[9][9]) {
+    bool completed = true;
+    // Try every candidate by recursively deepening into possible configurations
+    for (int row = 0; row < 9; ++row) {
+        for (int col = 0; col < 9; ++col) {
+            if (!isSudokuDigit(grid[row][col])) {
+                completed = false;
+                Candidates & cands = gridC[row][col];
+                for (int i = 0; i < 9; ++i) {
+                    if (cands.possible[i]) {
+                        // Make copies
+                        char x_grid[9][9];
+                        Candidates x_gridC[9][9];
+                        for (int r = 0; r < 9; ++r) {
+                            for (int c = 0; c < 9; ++c) {
+                                x_grid[r][c] = grid[r][c];
+                                x_gridC[r][c] = gridC[r][c];
+                            }
+                        }
+                        x_grid[row][col] = i + '1';
+                        std::list<std::pair<int, int>> pointsOfInterest;
+                        pointsOfInterest.push_back(
+                            std::pair<int, int>(row, col));
+                        while (!pointsOfInterest.empty()) {
+                            int row = pointsOfInterest.front().first;
+                            int col = pointsOfInterest.front().second;
+                            pointsOfInterest.pop_front();
+                            auto r = process(row, col, x_grid, x_gridC,
+                                             pointsOfInterest);
+                            if (-1 == r) { return -1; }
+                        }
+                        if (1 == trySolve(x_grid, x_gridC, finalGrid)) {
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (completed) {
+        commit(grid, finalGrid);
+        return 1;
+    }
+    return 0;
+}
+
 int solve(char grid[9][9]) {
+    // Begin by constraining possibilities (some easier puzzles can be solved
+    // completely merely with this method)
     Candidates gridC[9][9];
     std::list<std::pair<int, int>> pointsOfInterest;
     for (int row = 0; row < 9; ++row) {
@@ -108,7 +178,9 @@ int solve(char grid[9][9]) {
         pointsOfInterest.pop_front();
         process(row, col, grid, gridC, pointsOfInterest);
     }
-    printNumPossibilities(gridC);
+    // Recursively descend and entertain possibilities from our constrained
+    // sample space (further constraining on copies of candidates)
+    trySolve(grid, gridC, grid);
     return 0;
 }
 
@@ -135,23 +207,35 @@ int main() {
                         {'-', '-', '-',  '-', '-', '-',  '-', '-', '-'},
                         {'-', '-', '-',  '-', '-', '-',  '-', '-', '-'} };
     */
-    // This puzzle will be solved by constraining the search space
-    char grid[9][9] = { {'-', '-', '3',  '-', '2', '-',  '6', '-', '-'},
-                        {'9', '-', '-',  '3', '-', '5',  '-', '-', '1'},
-                        {'-', '-', '1',  '8', '-', '6',  '4', '-', '-'},
+    // Arto Inkala's 2010 puzzle
+    char grid[9][9] = { {'-', '-', '5',  '3', '-', '-',  '-', '-', '-'},
+                        {'8', '-', '-',  '-', '-', '-',  '-', '2', '-'},
+                        {'-', '7', '-',  '-', '1', '-',  '5', '-', '-'},
 
-                        {'-', '-', '8',  '1', '-', '2',  '9', '-', '-'},
-                        {'7', '-', '-',  '-', '-', '-',  '-', '-', '8'},
-                        {'-', '-', '6',  '7', '-', '8',  '2', '-', '-'},
+                        {'4', '-', '-',  '-', '-', '5',  '3', '-', '-'},
+                        {'-', '1', '-',  '-', '7', '-',  '-', '-', '6'},
+                        {'-', '-', '3',  '2', '-', '-',  '-', '8', '-'},
 
-                        {'-', '-', '2',  '6', '-', '9',  '5', '-', '-'},
-                        {'8', '-', '-',  '2', '-', '3',  '-', '-', '9'},
-                        {'-', '-', '5',  '-', '1', '-',  '3', '-', '-'} };
+                        {'-', '6', '-',  '5', '-', '-',  '-', '-', '9'},
+                        {'-', '-', '4',  '-', '-', '-',  '-', '3', '-'},
+                        {'-', '-', '-',  '-', '-', '9',  '7', '-', '-'} };
+    // Puzzle can be solved by recursively entertaining all solutions but it
+    // will be benefited by constraining the search space before guess and
+    // checking every plausible option
     printGrid(grid);
     solve(grid);
     printGrid(grid);
-    // Not all puzzles can be solved with this method
-    // TODO Use a recursive DFS trial of each possible filler to solve all
-    // problems
+    char gridAnswer[9][9] = { {'1', '4', '5',  '3', '2', '7',  '6', '9', '8'},
+                              {'8', '3', '9',  '6', '5', '4',  '1', '2', '7'},
+                              {'6', '7', '2',  '9', '1', '8',  '5', '4', '3'},
+
+                              {'4', '9', '6',  '1', '8', '5',  '3', '7', '2'},
+                              {'2', '1', '8',  '4', '7', '3',  '9', '5', '6'},
+                              {'7', '5', '3',  '2', '9', '6',  '4', '8', '1'},
+
+                              {'3', '6', '7',  '5', '4', '2',  '8', '1', '9'},
+                              {'9', '8', '4',  '7', '6', '1',  '2', '3', '5'},
+                              {'5', '2', '1',  '8', '3', '9',  '7', '6', '4'} };
+    assert(matches(grid, gridAnswer));
     return 0;
 }
