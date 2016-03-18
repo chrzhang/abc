da9f16275a8ce39f7b5af673bcc167d7d6198228
Christopher Zhang <christopherzhang3@gmail.com>
2016-03-18 00:30:46 -0400

    Remove object files and executables left in any subdirectory.

---
+++ clean.py
@@ -0,0 +1,8 @@
+import os
+
+for dirpath, dirnames, filenames in os.walk("."):
+    for f in filenames:
+        if f.endswith(".o"):
+            os.remove(dirpath + "/" + f)
+        elif f == "p":
+            os.remove(dirpath + "/" + f)
