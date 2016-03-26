4070a29504f73922b8a09e7d9bfe6a22c6de9f8c
Christopher Zhang <christopherzhang3@gmail.com>
2016-03-26 14:59:03 -0400

    Add additional test cases for rectangle area calculations.

--- geometry/rectangleArea/main.cpp
+++ geometry/rectangleArea/main.cpp
@@ -4,8 +4,8 @@
 
 // Find the combined area of two rectangles
 
-// The lower left of the first rectangle is at (A,B), the upper right at (C, D)
-// The lower left of the other rectangle is at (E,F), the upper right at (G, H)
+// The lower left of the first rectangle is at (A,B), the upper right at (C,D)
+// The lower left of the other rectangle is at (E,F), the upper right at (G,H)
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
     // Add the areas of the rectangles and subtract the overlap
     int a1 = (C - A) * (D - B);
@@ -20,5 +20,8 @@ int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
 
 int main() {
     assert(computeArea(-3, 0, 3, 4, 0, -1, 9, 2)  == 6 * 4 + 9 * 3 - 6);
+    assert(computeArea(0, -1, 9, 2, -3, 0, 3, 4)  == 6 * 4 + 9 * 3 - 6);
+    assert(computeArea(0, 0, 0, 0, 0, -1, 9, 2)  == 9 * 3);
+    assert(computeArea(-1, -1, 1, 1, -1, -1, 1, 1)  == 2 * 2);
     return 0;
 }
