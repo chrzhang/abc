25b25f6c6e197c126a12ab94107db47e52762fc5
Christopher Zhang <christopherzhang3@gmail.com>
2016-04-21 00:55:01 -0400

    Find weekday of a given date between 1990 and 1999.

--- misc/findWeekdayOfDate/main.cpp
+++ misc/findWeekdayOfDate/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cassert>
 
 // Find the weekday of a given date in [1990, 1999]
+
 enum Month { january, february, march, april, may, june, july, august,
              september, october, november, december };
 
@@ -59,22 +61,95 @@ void next(Day & d) {
     }
 }
 
-Day incDay(Day d, int amt) {
+void next(Month & m) {
+    switch (m) {
+        case january:
+            m = february;
+            break;
+        case february:
+            m = march;
+            break;
+        case march:
+            m = april;
+            break;
+        case april:
+            m = may;
+            break;
+        case may:
+            m = june;
+            break;
+        case june:
+            m = july;
+            break;
+        case july:
+            m = august;
+            break;
+        case august:
+            m = september;
+            break;
+        case september:
+            m = october;
+            break;
+        case october:
+            m = november;
+            break;
+        case november:
+            m = december;
+            break;
+        case december:
+            m = january;
+            break;
+    }
+}
+
+void incDay(Day & d, int amt) {
     for (int i = 0; i < amt % 7; ++i) {
         next(d);
     }
-    return d;
 }
 
-Day getWeekDay(Month m, Day d, int y) {
-    return monday; // Placeholder
+bool isLeapYear(int y) {
+    return y == 1992 || y == 1996;
+}
+
+Day getWeekDay(Month m, int d, int y) {
+    if (y < 1990 || y > 1999) {
+        std::cout << "Year " << y << " not in range [1990, 1999].\n";
+        assert(false);
+    }
+    int daysInEachMonthYear[10][12];
+    for (int year = 1990; year < 2000; ++year) {
+        daysInEachMonthYear[year - 1990][january] =
+        daysInEachMonthYear[year - 1990][march] =
+        daysInEachMonthYear[year - 1990][may] =
+        daysInEachMonthYear[year - 1990][july] =
+        daysInEachMonthYear[year - 1990][august] =
+        daysInEachMonthYear[year - 1990][october] =
+        daysInEachMonthYear[year - 1990][december] = 31;
+        daysInEachMonthYear[year - 1990][april] =
+        daysInEachMonthYear[year - 1990][june] =
+        daysInEachMonthYear[year - 1990][september] =
+        daysInEachMonthYear[year - 1990][november] = 30;
+        daysInEachMonthYear[year - 1990][february] = isLeapYear(year) ? 29 : 28;
+    }
+    int year = 1990;
+    int daysPassed = 0;
+    while (year != y) {
+        for (int i = 0; i < 12; ++i) {
+            daysPassed += daysInEachMonthYear[year - 1990][i];
+        }
+        ++year;
+    }
+    for (Month mi = january; mi != m; next(mi)) {
+        daysPassed += daysInEachMonthYear[y - 1990][mi];
+    }
+    daysPassed += d - 1;
+    Day start = monday;
+    incDay(start, daysPassed);
+    return start;
 }
 
 int main() {
-    Day d = monday;
-    for (int i = 0; i < 20; ++i) {
-        std::cout << d << std::endl;
-        next(d);
-    }
+    assert(monday == getWeekDay(april, 26, 1999));
     return 0;
 }
