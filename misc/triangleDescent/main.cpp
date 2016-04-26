b86abd9cbe0037ef68c717820b3a00d9f00e9a1d
Christopher Zhang <christopherzhang3@gmail.com>
2016-04-25 22:40:49 -0400

    Be able to find the left and right children of a node in the triangle.

--- misc/triangleDescent/main.cpp
+++ misc/triangleDescent/main.cpp
@@ -4,19 +4,47 @@
 // Finds path from top of a triangle to the base with the max possible sum
 
 struct Triangle {
-    std::vector<int> vec;
-    Triangle(const std::vector<int> & v) : vec(v) {}
+    std::vector<std::pair<int, int>> vec;
+    Triangle(const std::vector<int> & v) {
+        auto it = v.begin();
+        for (int level = 1; it != v.end(); ++level) {
+            for (int i = 0; i < level && it != v.end(); ++i) {
+                vec.push_back(std::make_pair(level, *it));
+                ++it;
+            }
+        }
+    }
+    int getLeftChildIndex(int i) const {
+        if (i < 0 || i >= (int) vec.size()) { return -1; }
+        const std::pair<int, int> & p = vec[i];
+        if (i + p.first >= (int) vec.size()) { return -1; }
+        return i + p.first;
+    }
+    int getRightChildIndex(int i) const {
+        if (i < 0 || i >= (int) vec.size()) { return -1; }
+        const std::pair<int, int> & p = vec[i];
+        if (i + p.first + 1 >= (int) vec.size()) { return -1; }
+        return i + p.first + 1;
+    }
 };
 
+std::ostream & operator<<(std::ostream & os, const std::pair<int, int> & p) {
+    if (p.first == -1) { os << "-"; }
+    else { os << "(" /*<< p.first << ", "*/ << p.second << ")"; }
+    return os;
+}
+
 std::ostream & operator<<(std::ostream & os, const Triangle & t) {
-    auto it = t.vec.begin();
-    for (int level = 1; ; ++level) {
-        for (int i = 0; i < level && it != t.vec.end(); ++i) {
-            os << *it << " ";
-            ++it;
-        }
-        if (it == t.vec.end()) { break; }
-        os << "\n";
+    int i = 0;
+    for (auto p : t.vec) {
+        os << p;
+        int lci = t.getLeftChildIndex(i);
+        int rci = t.getRightChildIndex(i);
+        std::pair<int, int> dummy(-1, -1);
+        os << "\tLeft child: " << ((lci == -1) ? dummy : t.vec[lci])
+           << "\tRight child: " << ((lci == -1) ? dummy : t.vec[rci])
+           << std::endl;
+        ++i;
     }
     return os;
 }
