082d7a5c44a34f30442a5ee412109d1f4c706cb0
Christopher Zhang <christopherzhang3@gmail.com>
2016-05-10 12:18:05 -0400

    Decrypt with Vigenere cipher.

--- ciphers/vigenere/main.cpp
+++ ciphers/vigenere/main.cpp
@@ -42,7 +42,32 @@ std::string encode(const std::string & str, const std::string & key) {
     return result;
 }
 
+std::string decode(const std::string & str, const std::string & key) {
+    if (str.empty() || key.empty()) { return ""; }
+    for (auto c : str) {
+        if (!isalpha(c)) { return ""; }
+        if (!isupper(c)) { return ""; }
+    }
+    for (auto c : key) {
+        if (!isalpha(c)) { return ""; }
+        if (!isupper(c)) { return ""; }
+    }
+    // String validated
+    std::string result;
+    result.reserve(str.size());
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        char keyChar = key[(it - str.begin()) % key.size()];
+        int diff = *it - keyChar;
+        if (keyChar > *it) { // Need wraparound
+            diff = 26 - (keyChar - *it);
+        }
+        result.push_back(diff + 'A');
+    }
+    return result;
+}
+
 int main() {
     assert("VCSGQRHVKGJVGOW" == encode("TOPCODERISGREAT", "CODE"));
+    assert("TOPCODERISGREAT" == decode("VCSGQRHVKGJVGOW", "CODE"));
     return 0;
 }
