94b68b8c4c4c7541186c557141150e6f317928d1
Christopher Zhang <christopherzhang3@gmail.com>
2016-05-10 11:48:36 -0400

    Encrypt with Vigenere cipher.

---
+++ ciphers/vigenere/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+/*
+ Vigenere Cipher
+ A code word (key) is written repeatedly over a message
+ Plaintext: hello
+ Key: bye
+ Repeated:  byeby
+ For each letter in the plaintext, find the corresponding repeated-key letter
+ and encrypt by finding intersection in table formed by staggered alphabet
+ (rows are letters in code, columns are letters in plaintext)
+   A B C D ... Z
+ A A B C D ... Z
+ B B C D E ... A
+ C C D E F ... B
+ D D E F G ... C
+ . . . . .     .
+ . . . . .     .
+ . . . . .     .
+ Z Z A B C ... Y
+*/
+
+std::string encode(const std::string & str, const std::string & key) {
+    if (str.empty() || key.empty()) { return ""; }
+    for (auto c : str) {
+        if (!isalpha(c)) { return ""; }
+        if (!isupper(c)) { return ""; }
+    }
+    for (auto c : key) {
+        if (!isalpha(c)) { return ""; }
+        if (!isupper(c)) { return ""; }
+    }
+    // String validated
+    std::string result;
+    result.reserve(str.size());
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        char keyChar = key[(it - str.begin()) % key.size()];
+        result.push_back((char)((((keyChar - 'A') + *it) - 'A') % 26 + 'A'));
+    }
+    return result;
+}
+
+int main() {
+    assert("VCSGQRHVKGJVGOW" == encode("TOPCODERISGREAT", "CODE"));
+    return 0;
+}
