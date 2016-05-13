7ff2b48485054653495538dd623ac8a5d8eb2d8f
Christopher Zhang <christopherzhang3@gmail.com>
2016-05-13 17:07:01 -0400

    Read in class names from a validated input string.

--- sorts/topological/main.cpp
+++ sorts/topological/main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <set>
+#include <algorithm>
 
 // Given classes that have pre-requisites, find a valid ordering to take the
 // classes (if there is no strict ordering between two or more classes, use the
-// class number to order and then if the numbers match, the alphabetical order
+// class number to order and then if the numbers match, the lexicographic order
 // of the department)
 
 struct Class {
     std::string dept;
     int num;
-    Class(const std::string & s, int n) : dept(s), num(n) {}
+    Class(const std::string & s) {
+        auto it = s.begin();
+        while (isalpha(*it) && isupper(*it)) { ++it; }
+        dept = std::string(s.begin(), it);
+        num = std::stoi(std::string(it, s.end()));
+    }
 };
 
-// 3 or 4 characters of capital letters
+bool operator<(const Class & c1, const Class & c2) {
+    if (c1.num != c2.num) {
+        return c1.num < c2.num;
+    } else {
+        return std::lexicographical_compare(c1.dept.begin(), c1.dept.end(),
+                                            c2.dept.begin(), c2.dept.end());
+    }
+}
+
+// Departments are 3 or 4 characters of capital letters
 bool isValidDept(const std::string & s) {
     if (s.size() < 3 || s.size() > 4) { return false; }
     for (auto c : s) {
@@ -22,6 +38,7 @@ bool isValidDept(const std::string & s) {
     return true;
 }
 
+// Class numbers are in [100, 999]
 bool isValidNum(int n) {
     return n >= 100 && n <= 999;
 }
@@ -33,15 +50,73 @@ bool isValidListing(const std::string & s) {
     return isValidNum(std::stoi(std::string(it, s.end())));
 }
 
+bool isValidInputLine(const std::string & s) {
+    if (s.empty()) { return false; }
+    bool colonFound = false;
+    auto it = s.begin();
+    while (it != s.end()) {
+        if (*it == ':') {
+            if (!isValidListing(std::string(s.begin(), it))) { return false; }
+            colonFound = true;
+            ++it;
+            break;
+        }
+        ++it;
+    }
+    if (!colonFound) { return false; }
+    if (it == s.end()) { return true; }
+    while (it != s.end()) {
+        if (*it != ' ') { return false; }
+        ++it;
+        auto endit = it;
+        while (endit != s.end() && *endit != ' ') { ++endit; }
+        if (!isValidListing(std::string(it, endit))) { return false; }
+        it = endit;
+    }
+    return true;
+}
+
+void readValidInputLine(const std::string & s, std::set<Class> & classes) {
+    auto it = s.begin();
+    while (it != s.end()) {
+        if (*it == ':') {
+            classes.insert(Class(std::string(s.begin(), it)));
+            ++it;
+            break;
+        }
+        ++it;
+    }
+    while (it != s.end()) {
+        ++it;
+        auto endit = it;
+        while (endit != s.end() && *endit != ' ') { ++endit; }
+        classes.insert(Class(std::string(it, endit)));
+        it = endit;
+    }
+}
+
 int main() {
-    assert(isValidListing("CSE111"));
-    assert(isValidListing("CSE110"));
-    assert(isValidListing("MATH101"));
-    assert(!isValidListing("CS117"));
-    assert(!isValidListing("cs117"));
-    assert(!isValidListing("CS9E11"));
-    assert(!isValidListing("MAM2222"));
-    assert(!isValidListing("MAM22"));
-    assert(!isValidListing("ENGIN517"));
+    assert(isValidInputLine("CSE111: CSE110 MATH101"));
+    assert(isValidInputLine("CSE110:"));
+    assert(!isValidInputLine("CS117:"));
+    assert(!isValidInputLine("cs117:"));
+    assert(!isValidInputLine("CS9E11:"));
+    assert(!isValidInputLine("CSE110: "));
+    assert(!isValidInputLine("CSE110: CSE101 "));
+    assert(!isValidInputLine("MATH211: MAM2222"));
+    assert(!isValidInputLine("MATH211: MAM22"));
+    assert(!isValidInputLine("ENGIN517: MATH211"));
+    {
+        std::set<Class> classes;
+        auto classSchedule = { "CSE121: CSE110",
+                               "CSE110:",
+                               "MATH122:" };
+        for (auto cl : classSchedule) {
+            readValidInputLine(cl, classes);
+        }
+        for (auto c : classes) {
+            std::cout << c.dept << c.num << std::endl;
+        }
+    }
     return 0;
 }
