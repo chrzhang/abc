8cd306fb260b207dbed0ed343597749aee34cbc5
Christopher Zhang <christopherzhang3@gmail.com>
2016-06-11 17:23:16 -0400

    Accept a pointer arg, modify the variable, and return the destination as a reference.

--- misc/references/main.cpp
+++ misc/references/main.cpp
@@ -1,5 +1,10 @@
 #include <cassert>
 
+char & func(char * c_ptr) {
+    *c_ptr = 'z';
+    return *c_ptr;
+}
+
 int main() {
     // Uninitialized reference is not allowed
     // char & c;
@@ -12,5 +17,10 @@ int main() {
     assert(e == 'e'); // It didn't work
     // Null references cannot exist in a well-defined program because
     // de-referencing it would be the undefined de-referencing of a null pointer
+    char c = 'a';
+    char & c_r = func(&c);
+    assert(c_r == 'z'); // Make sure char was modified
+    c_r = 'b'; // Make sure return value is still a reference
+    assert(c == 'b');
     return 0;
 }
