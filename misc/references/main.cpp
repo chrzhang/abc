74f4973c397fdba660dbcb951b53d710b4ae4c80
Christopher Zhang <christopherzhang3@gmail.com>
2016-06-11 17:32:12 -0400

    Modify a variable via a reference.

--- misc/references/main.cpp
+++ misc/references/main.cpp
@@ -5,6 +5,10 @@ char & func(char * c_ptr) {
     return *c_ptr;
 }
 
+void mod(char & arg) {
+    arg = 'y';
+}
+
 int main() {
     // Uninitialized reference is not allowed
     // char & c;
@@ -17,10 +21,15 @@ int main() {
     assert(e == 'e'); // It didn't work
     // Null references cannot exist in a well-defined program because
     // de-referencing it would be the undefined de-referencing of a null pointer
+    // Modify variable via a pointer and return as a reference
     char c = 'a';
     char & c_r = func(&c);
     assert(c_r == 'z'); // Make sure char was modified
     c_r = 'b'; // Make sure return value is still a reference
     assert(c == 'b');
+    // Modify variable via a reference
+    char g = 'g';
+    mod(g);
+    assert(g == 'y');
     return 0;
 }
