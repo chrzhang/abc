547d784c30c6f6eb582fcf6051191d093078ef66
Christopher Zhang <christopherzhang3@gmail.com>
2016-07-23 20:24:27 -0400

    Document logic of linear wiggly algorithm.

--- arrays/wiggleSubsequence/main.cpp
+++ arrays/wiggleSubsequence/main.cpp
@@ -6,6 +6,57 @@
 // Given a sequence of numbers, find the length of the longest subsequence such
 // that traversing the value alternates between increase and decrease
 
+/*
+All sequences of length smaller than 2 are trivially wiggly. For the general
+case, a number under consideration is Current, with a Previous and Next number.
+In terms of ordering 3 numbers, there are 3! possibilites of greatest to least
+ordering with the case of equal ordering able to be generalized into the
+ignored cases below.
+
+C - Current
+N - Next
+P - Previous
+
+6 Possible orderings (Higher is greater)
+
+1. C can be safely ignored without affecting wiggly max length
+P
+ \
+  C
+   \
+    N
+
+2. C is included to calculate wiggly max length
+P
+ \   N
+  \ /
+   C
+
+3. C is included to calculate wiggly max length
+   C
+  / \
+ /   N
+P
+
+4. C is included to calculate wiggly max length
+  C
+ / \
+P   \
+     N
+
+5. C can be safely ignored without affecting wiggly max length
+    N
+   /
+  C
+ /
+P
+
+6. C is included to calculate wiggly max length
+     N
+P   /
+ \ /
+  C
+*/
 size_t wiggleMaxLength(const std::vector<int> & numbers) {
     if (numbers.size() < 2) {
         return numbers.size();
@@ -13,6 +64,7 @@ size_t wiggleMaxLength(const std::vector<int> & numbers) {
     size_t count = 1;
     enum SEEKING { ANY, UP, DOWN };
     SEEKING target = ANY;
+    // Start at second element so we have a Previous, Current, and Next number
     for (size_t i = 1; i < numbers.size(); ++i) {
         if (numbers[i - 1] < numbers[i] &&
             (target == ANY || target == DOWN)) {
@@ -23,6 +75,7 @@ size_t wiggleMaxLength(const std::vector<int> & numbers) {
             ++count;
             target = DOWN;
         }
+        // Otherwise, safely ignore
     }
     return count;
 }
