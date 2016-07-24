6c0decf6664d3cdf6c75a3ff14b032bcfc1abdba
Christopher Zhang <christopherzhang3@gmail.com>
2016-07-23 20:06:21 -0400

    Simplify algorithm to linear complexity.

--- arrays/wiggleSubsequence/main.cpp
+++ arrays/wiggleSubsequence/main.cpp
@@ -6,46 +6,25 @@
 // Given a sequence of numbers, find the length of the longest subsequence such
 // that traversing the value alternates between increase and decrease
 
-// Add the number to the subsequence if it would result in a series of
-// alternating increases and decreases
-int addIfWiggle(std::vector<int> & subsequence, int num) {
-    if (subsequence.empty()) {
-        subsequence.push_back(num);
-        return 0;
-    } else if (subsequence.size() == 1 && num != subsequence[0]) {
-        subsequence.push_back(num);
-        return 0;
-    }
-    auto rit = subsequence.rbegin();
-    if ((*rit > *std::next(rit) && num < *rit) ||
-        (*rit < *std::next(rit) && num > *rit)) {
-        subsequence.push_back(num);
-        return 0;
-    }
-    return 1;
-}
-
-// Helper recursive function that explores all possibilities of including or
-// not including a number in the subsequence.
-void wiggleMaxLengthAux(const std::vector<int> & numbers, size_t currentIndex,
-                        size_t & max, std::vector<int> currSubsequence) {
-    if (currentIndex >= numbers.size()) {
-        max = std::max(max, currSubsequence.size());
-        return;
-    }
-    // Ignore the current index
-    wiggleMaxLengthAux(numbers, currentIndex + 1, max, currSubsequence);
-    // Include the current index
-    if (0 == addIfWiggle(currSubsequence, numbers[currentIndex])) {
-        wiggleMaxLengthAux(numbers, currentIndex + 1, max, currSubsequence);
-    }
-}
-
-
 size_t wiggleMaxLength(const std::vector<int> & numbers) {
-    size_t max = 0;
-    wiggleMaxLengthAux(numbers, 0, max, std::vector<int>());
-    return max;
+    if (numbers.size() < 2) {
+        return numbers.size();
+    }
+    size_t count = 1;
+    enum SEEKING { ANY, UP, DOWN };
+    SEEKING target = ANY;
+    for (size_t i = 1; i < numbers.size(); ++i) {
+        if (numbers[i - 1] < numbers[i] &&
+            (target == ANY || target == DOWN)) {
+            ++count;
+            target = UP;
+        } else if (numbers[i - 1] > numbers[i]
+                   && (target == ANY || target == UP)) {
+            ++count;
+            target = DOWN;
+        }
+    }
+    return count;
 }
 
 void test() {
@@ -58,6 +37,7 @@ void test() {
     assert(wiggleMaxLength({1, 17, 5, 10, 13, 15, 10, 5, 16, 8}) == 7);
                          // ^  ^      ^   ^       ^      ^   ^
     assert(wiggleMaxLength({1, 2, 3, 4, 5, 6, 7, 8, 9}) == 2);
+    assert(wiggleMaxLength({33, 53, 12, 64, 50, 41, 45, 21, 97, 35, 47}) == 10);
 }
 
 int main() {
