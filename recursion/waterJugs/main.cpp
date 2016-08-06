4ef23f1e1eacce1cd306f27169a90a15e9c1d17b
Christopher Zhang <christopherzhang3@gmail.com>
2016-08-06 02:12:46 -0400

    Print out steps used to achieve target volume.

--- recursion/waterJugs/main.cpp
+++ recursion/waterJugs/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <set>
+#include <list>
 #include <utility>
 #include <algorithm>
 #include <cassert>
@@ -21,6 +22,13 @@ struct BucketState {
     }
 };
 
+enum Action { FILL_FIRST_BUCKET,
+              FILL_SECOND_BUCKET,
+              EMPTY_FIRST_BUCKET,
+              EMPTY_SECOND_BUCKET,
+              POUR_FIRST_INTO_SECOND_BUCKET,
+              POUR_SECOND_INTO_FIRST_BUCKET };
+
 bool operator<(const BucketState & stateA, const BucketState & stateB) {
     return std::tie(stateA.amt1, stateA.amt2) <
            std::tie(stateB.amt1, stateB.amt2);
@@ -30,71 +38,99 @@ bool exploreFromState(const BucketState currentState,
                       const int bucket1_capacity,
                       const int bucket2_capacity,
                       const int target,
-                      std::set<BucketState> & exploredStates) {
+                      std::set<BucketState> & exploredStates,
+                      std::list<Action> actions) {
     if (exploredStates.find(currentState) != exploredStates.end()) {
         return false;
     }
     exploredStates.insert(currentState);
     if (currentState.sum() == target) {
+        for (auto a : actions) {
+            if (a == FILL_FIRST_BUCKET) {
+                std::cout << "Fill the first bucket.\n";
+            } else if (a == FILL_SECOND_BUCKET) {
+                std::cout << "Fill the second bucket.\n";
+            } else if (a == EMPTY_FIRST_BUCKET) {
+                std::cout << "Empty the first bucket.\n";
+            } else if (a == EMPTY_SECOND_BUCKET) {
+                std::cout << "Empty the second bucket.\n";
+            } else if (a == POUR_FIRST_INTO_SECOND_BUCKET) {
+                std::cout << "Pour the first bucket into the second bucket.\n";
+            } else if (a == POUR_SECOND_INTO_FIRST_BUCKET) {
+                std::cout << "Pour the second bucket into the first bucket.\n";
+            }
+        }
         return true;
     }
     // Fill the first bucket
     if (currentState.amt1 != bucket1_capacity) {
+        actions.push_back(FILL_FIRST_BUCKET);
         if (exploreFromState(BucketState(bucket1_capacity, currentState.amt2),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     // Fill the second bucket
     if (currentState.amt2 != bucket2_capacity) {
+        actions.push_back(FILL_SECOND_BUCKET);
         if (exploreFromState(BucketState(currentState.amt1, bucket2_capacity),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     // Empty the first bucket
     if (currentState.amt1 != 0) {
+        actions.push_back(EMPTY_FIRST_BUCKET);
         if (exploreFromState(BucketState(0, currentState.amt2),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     // Empty the second bucket
     if (currentState.amt2 != 0) {
+        actions.push_back(EMPTY_SECOND_BUCKET);
         if (exploreFromState(BucketState(currentState.amt1, 0),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     // Pour first into second bucket
     if (currentState.amt2 != bucket2_capacity &&
         currentState.amt1 > 0) {
+        actions.push_back(POUR_FIRST_INTO_SECOND_BUCKET);
         int leftover = currentState.amt1 + currentState.amt2 - bucket2_capacity;
         if (exploreFromState(BucketState(std::max(0, leftover),
                                          std::min(bucket2_capacity,
                                                   currentState.amt1 +
                                                   currentState.amt2)),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     // Pour second into first bucket
     if (currentState.amt1 != bucket1_capacity &&
         currentState.amt2 > 0) {
+        actions.push_back(POUR_SECOND_INTO_FIRST_BUCKET);
         int leftover = currentState.amt1 + currentState.amt2 - bucket1_capacity;
         if (exploreFromState(BucketState(std::min(bucket1_capacity,
                                                   currentState.amt1 +
                                                   currentState.amt2),
                                          std::max(0, leftover)),
                              bucket1_capacity, bucket2_capacity,
-                             target, exploredStates)) {
+                             target, exploredStates, actions)) {
             return true;
         }
+        actions.pop_back();
     }
     return false;
 }
@@ -102,15 +138,17 @@ bool exploreFromState(const BucketState currentState,
 bool canContainExactly(const int bucket1_capacity,
                        const int bucket2_capacity,
                        const int target) {
-    if (target == 0 || target == bucket1_capacity + bucket2_capacity) {
-        return true;
-    }
+    std::cout << "\nProblem: -----\n";
+    std::cout << "Bucket 1 can hold " << bucket1_capacity << "\n";
+    std::cout << "Bucket 2 can hold " << bucket2_capacity << "\n";
+    std::cout << "Target is " << target << "\n";
     std::set<BucketState> exploredStates;
     return exploreFromState(BucketState(0, 0),
                             bucket1_capacity,
                             bucket2_capacity,
                             target,
-                            exploredStates);
+                            exploredStates,
+                            {});
 }
 
 int main() {
