31c59464db7fb33498d67df79c1837e711fce8e6
Christopher Zhang <christopherzhang3@gmail.com>
2016-10-21 23:17:21 -0400

    Remove dupes in solution.

--- maths/fourSum/main.cpp
+++ maths/fourSum/main.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <set>
 #include <algorithm>
+#include <array>
 
 // Find all a, b, c, d that sum to 0
 
@@ -13,13 +14,15 @@ bool nothing_shared_between(const std::pair<int, int> & p1,
 }
 
 void display(const std::vector<int> & vec) {
+    std::cout << "Data >> ";
     for (auto x : vec) {
         std::cout << x << " ";
     }
     std::cout << "\n";
 }
 
-void fourSum(const std::vector<int> & vec) {
+std::set<std::array<int, 4>> fourSum(const std::vector<int> & vec,
+                                     const int target) {
     display(vec);
     std::unordered_map<int, std::set<std::pair<int, int>>> sum_to_pairs;
     for (auto x = vec.begin(); x != vec.end(); ++x) {
@@ -28,21 +31,11 @@ void fourSum(const std::vector<int> & vec) {
                                           std::distance(vec.begin(), y)});
         }
     }
-    /*
-    for (auto entry : sum_to_pairs) {
-        std::cout << entry.first << " ";
-        for (auto pair : entry.second) {
-            std::cout << "(" << pair.first << ": " << vec[pair.first]
-                      << ", " << pair.second << ": " << vec[pair.second]
-                      << ") ";
-        }
-        std::cout << "\n";
-    }
-    */
+    std::set<std::array<int, 4>> results;
     for (auto entry : sum_to_pairs) {
         auto & curr_sum = entry.first;
         auto & curr_pairs = entry.second;
-        auto seek = sum_to_pairs.find(-1 * curr_sum);
+        auto seek = sum_to_pairs.find(target - curr_sum);
         if (seek != sum_to_pairs.end()) {
             // Make sure no values are re-used
             auto & inv_pairs = seek->second;
@@ -55,19 +48,20 @@ void fourSum(const std::vector<int> & vec) {
                                     vec[inv_pair.first],
                                     vec[inv_pair.second] } };
                             std::sort(values.begin(), values.end());
-                            // TODO Handle dupes in results
-                            std::cout << values[0] << " "
-                                      << values[1] << " "
-                                      << values[2] << " "
-                                      << values[3] << "\n";
+                            results.insert(values);
                     }
                 }
             }
         }
     }
+    for (auto r : results) {
+        std::cout << r[0] << " " << r[1] << " " << r[2] << " " << r[3] << "\n";
+    }
+    return results;
 }
 
 int main() {
-    fourSum({2, 2, 3, 4, 5, -1, -8});
+    fourSum({2, 2, 3, 4, 5, -1, -8}, 0);
+    fourSum({1,0,-1,0,-2,2}, 0);
     return 0;
 }
