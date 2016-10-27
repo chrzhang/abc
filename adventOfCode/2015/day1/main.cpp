8380ba258141b6f0005002ab716f9c95968d7371
Christopher Zhang <christopherzhang3@gmail.com>
2016-10-26 20:10:32 -0400

    Finish Day 1 of Advent of Code 2015.

--- adventOfCode/2015/day1/main.cpp
+++ adventOfCode/2015/day1/main.cpp
@@ -11,10 +11,17 @@ int main(int argc, char * argv[]) {
     if (f.is_open()) {
         char c;
         int curr_floor = 0;
+        size_t pos_enter_basement = 0;
+        size_t curr_pos = 1;
         while (f.get(c)) {
             switch (c) {
                 case ')':
                     curr_floor -= 1;
+                    // First character causing him to enter the basement
+                    if (pos_enter_basement == 0 &&
+                        curr_floor == -1) {
+                        pos_enter_basement = curr_pos;
+                    }
                     break;
                 case '(':
                     curr_floor += 1;
@@ -22,8 +29,11 @@ int main(int argc, char * argv[]) {
                 default:
                     break;
             }
+            ++curr_pos;
         }
         std::cout << "Final floor: " << curr_floor << std::endl;
+        std::cout << "Position of 1st char to enter basement: "
+                  << pos_enter_basement << std::endl;
     }
     return 0;
 }
