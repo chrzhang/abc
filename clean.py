725957b4568efe575fc16eaa950959141a5dc94b
Christopher Zhang <christopherzhang3@gmail.com>
2017-12-19 00:18:08 -0500

    Remove .hi files.

--- clean.py
+++ clean.py
@@ -4,5 +4,7 @@ for dirpath, dirnames, filenames in os.walk("."):
     for f in filenames:
         if f.endswith(".o"):
             os.remove(dirpath + "/" + f)
+        elif f.endswith(".hi"):
+            os.remove(dirpath + "/" + f)
         elif f == "p":
             os.remove(dirpath + "/" + f)
