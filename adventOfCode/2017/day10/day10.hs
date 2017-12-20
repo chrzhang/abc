fb9ee38683be0b52f3890bcab0237857d8682a01
Christopher Zhang <christopherzhang3@gmail.com>
2017-12-19 22:59:31 -0500

    Fix hlint suggestions for Day 10.

--- adventOfCode/2017/day10/day10.hs
+++ adventOfCode/2017/day10/day10.hs
@@ -1,5 +1,5 @@
 import Control.Exception
-import qualified Data.List
+import qualified Data.List (intercalate)
 import qualified Data.List.Split
 import qualified Data.Bits
 import qualified Data.Char
@@ -152,7 +152,7 @@ revrot :: Int -> [a] -> Int -> [a]
 revrot l xs i = rotate reved (-i)
                 where len = length xs
                       shift = take len $ drop i $ cycle xs
-                      reved = (reverse $ take l shift) ++ drop l shift
+                      reved = reverse (take l shift) ++ drop l shift
 
 data ListState = ListState {ci :: Int, xss :: [Int], sk :: Int, lss :: [Int],
                             li :: Int}
@@ -161,19 +161,19 @@ next :: ListState -> ListState
 next liststate = ListState nci nxs nsk (lss liststate) nli
     where nxs = revrot currlen (xss liststate) (ci liststate)
           nci = (ci liststate + currlen + sk liststate) `mod`
-                (length $ xss liststate)
+                length (xss liststate)
           nsk = sk liststate + 1
           nli = li liststate + 1
-          currlen = (lss liststate) !! (li liststate)
+          currlen = lss liststate !! li liststate
 
 day10a_solve :: [Int] -> [Int] -> Int
-day10a_solve a b = (xss fs !! 0) * (xss fs !! 1)
+day10a_solve a b = head (xss fs) * (xss fs !! 1)
                    where fs = finalstate a b
 
 finalstate :: [Int] -> [Int] -> ListState
-finalstate a b = (last sts)
-                  where sts = takeUntil (\x -> li x < length (lss x)) sts'
-                        sts' = iterate next (ListState 0 a 0 b 0)
+finalstate a b = last sts
+                 where sts = takeUntil (\x -> li x < length (lss x)) sts'
+                       sts' = iterate next (ListState 0 a 0 b 0)
 
 takeUntil :: (a -> Bool) -> [a] -> [a]
 takeUntil _ [] = []
@@ -194,12 +194,12 @@ sparsehash xs ls = xss fs
 densehash :: [Int] -> [Int]
 densehash xs
     | length xs /= 256 = error "Sparse hash length != 256"
-    | otherwise = map (\c -> foldr (Data.Bits.xor) 0 c) chunks
+    | otherwise = map (foldr Data.Bits.xor 0) chunks
                   where chunks = Data.List.Split.chunksOf 16 xs
 
 tohex :: [Int] -> String
-tohex xs = concat $ map (\x -> pad $ Numeric.showHex x "") xs
-           where pad s = if length s == 1 then '0':s else s
+tohex = concatMap (\x -> pad $ Numeric.showHex x "")
+        where pad s = if length s == 1 then '0':s else s
 
 day10b_solve :: [Int] -> [Int] -> String
 day10b_solve a b = tohex dh
