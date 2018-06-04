4af94fd5e673e14d1f75ead20c48b53176e76fd6
Christopher Zhang <christopherzhang3@gmail.com>
2018-06-04 09:27:03 -0700

    [LightMoreLight] Be clever about math.

--- uva/light_more_light/main.cpp
+++ uva/light_more_light/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <cassert>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 bool isItOn(const unsigned int nn) {
-    bool isItOn = false;
-    for (unsigned int ii = 1; ii <= nn; ++ii) {
-        if (nn % ii == 0) {
-            isItOn = !isItOn;
-        }
-    }
-    return isItOn;
+    // Switch gets toggled for every factor it has
+    // To count factors, take advantage of the fact that there
+    // are an equal number of factors on either side of the square root
+    // Because every factor is in a pair, the switch is toggled twice and
+    // nothing happens
+    // This problem then boils down to looking at the square root (which has
+    // no pair)
+    // If the sqrt is integral, then there was one toggle and the switch is on
+    return sqrt(nn) == (int) sqrt(nn);
 }
 
 string solve(const unsigned int nn) {
@@ -22,6 +25,8 @@ int main() {
     assert("no" == solve(3));
     assert("yes" == solve(6241));
     assert("no" == solve(8191));
+    assert("no" == solve(4294967295));
+    assert("yes" == solve(4294705156));
     ifstream inFile("input.txt");
     unsigned int xx;
     while (inFile >> xx) {
