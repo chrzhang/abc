fb320765e5880e82c354e20e5240f5ce736f6952
Christopher Zhang <christopherzhang3@gmail.com>
2018-06-10 22:04:49 -0400

    [Bridge] Always pick optimal pairing to avoid recursing 2^n possibilities. Properly erase from multiset.

--- uva/bridge/main.cpp
+++ uva/bridge/main.cpp
@@ -19,22 +19,18 @@ void printSteps(const vector<vector<int>> & steps) {
     }
 }
 
-void solveAux(const multiset<int> uncrossed, const multiset<int> crossed,
-              const int totalTime, int & lowestTotalTime,
-              const vector<vector<int>> & steps,
+void solveAux(multiset<int> uncrossed,
+              int & lowestTotalTime,
               vector<vector<int>> & lowestTotalSteps) {
-    if (totalTime > lowestTotalTime) {
-        return;
-    }
     if (uncrossed.empty()) {
-        if (totalTime < lowestTotalTime) {
-            lowestTotalTime = totalTime;
-            lowestTotalSteps = steps;
-        }
+        lowestTotalTime = 0;
+        lowestTotalSteps = {};
         return;
     }
     if (uncrossed.size() == 1) {
-        throw invalid_argument("");
+        lowestTotalTime = *uncrossed.begin();
+        lowestTotalSteps.push_back({*uncrossed.begin()});
+        return;
     }
     // The 'obvious' approach is to have the fastest person ferry each person
     // across. However, this means the two slowest people go individually and
@@ -42,72 +38,72 @@ void solveAux(const multiset<int> uncrossed, const multiset<int> crossed,
     // this is no longer the best approach since sending them together would
     // save more time.
     // In either cases, having the fastest person on the other side after a
-    // crossing return the flashlight is optimal
-    // Entertain both strategies
-    const bool peopleAlreadyCrossed = !crossed.empty();
-    const int fastestCrossed = *crossed.begin();
-    const int fastestUncrossed = *uncrossed.begin();
-    const int secondFastestUncrossed = *next(uncrossed.begin());
-    const int personSentBackIf2FastestWent =
-        (peopleAlreadyCrossed ? min(fastestCrossed, fastestUncrossed) : fastestUncrossed);
-    int tFor2Fastest =
-        max(fastestUncrossed, secondFastestUncrossed) + personSentBackIf2FastestWent;
-    const int slowestUncrossed = *uncrossed.rbegin();
-    const int secondSlowestUncrossed = *next(uncrossed.rbegin());
-    const int personSentBackIf2SlowestWent =
-        (peopleAlreadyCrossed ? min(*crossed.begin(), secondSlowestUncrossed) : secondSlowestUncrossed);
-    int tFor2Slowest =
-        max(slowestUncrossed, secondSlowestUncrossed) + personSentBackIf2SlowestWent;
-    { // Send the two fastest
-        auto fast_steps = steps;
-        fast_steps.push_back({fastestUncrossed, secondFastestUncrossed});
-        auto fast_uncrossed = uncrossed;
-        fast_uncrossed.erase(fast_uncrossed.begin());
-        fast_uncrossed.erase(fast_uncrossed.begin());
-        auto fast_crossed = crossed;
-        fast_crossed.emplace(fastestUncrossed);
-        fast_crossed.emplace(secondFastestUncrossed);
-        if (!fast_uncrossed.empty()) {
-            fast_steps.push_back({personSentBackIf2FastestWent});
-            fast_crossed.erase(personSentBackIf2FastestWent);
-            fast_uncrossed.emplace(personSentBackIf2FastestWent);
+    // crossing return the flashlight is optimal.
+    while (uncrossed.size() > 2) {
+        const int fastestUncrossed = *uncrossed.begin();
+        const int secondFastestUncrossed = *next(uncrossed.begin());
+        const int slowestUncrossed = *uncrossed.rbegin();
+        const int secondSlowestUncrossed = *(next(uncrossed.rbegin()));
+        const int tToFerrySlowestIndividually =
+            slowestUncrossed + fastestUncrossed + secondSlowestUncrossed + (uncrossed.size() > 3 ? fastestUncrossed : 0);
+        const int tToFerrySlowestTogether = // Sending the two fastest first is necessary
+            secondFastestUncrossed + fastestUncrossed + slowestUncrossed + (uncrossed.size() > 3 ? secondFastestUncrossed : 0);
+        if (tToFerrySlowestIndividually <= tToFerrySlowestTogether) {
+            lowestTotalSteps.push_back({fastestUncrossed, slowestUncrossed});
+            lowestTotalSteps.push_back({fastestUncrossed});
+            lowestTotalSteps.push_back({fastestUncrossed, secondSlowestUncrossed});
+            if (uncrossed.size() > 3) {
+                lowestTotalSteps.push_back({fastestUncrossed});
+            } else {
+                uncrossed.erase(uncrossed.begin()); // fastestUncrossed
+            }
+            lowestTotalTime += tToFerrySlowestIndividually;
+            uncrossed.erase(prev(uncrossed.end())); // slowestUncrossed
+            uncrossed.erase(prev(uncrossed.end())); // secondSlowestUncrossed
         } else {
-            tFor2Fastest -= personSentBackIf2FastestWent;
+            lowestTotalSteps.push_back({fastestUncrossed, secondFastestUncrossed});
+            lowestTotalSteps.push_back({fastestUncrossed});
+            lowestTotalSteps.push_back({secondSlowestUncrossed, slowestUncrossed});
+            if (uncrossed.size() > 3) {
+                lowestTotalSteps.push_back({secondFastestUncrossed});
+            } else {
+                uncrossed.erase(next(uncrossed.begin())); // secondFastestUncrossed
+            }
+            lowestTotalTime += tToFerrySlowestTogether;
+            uncrossed.erase(prev(uncrossed.end())); // slowestUncrossed
+            uncrossed.erase(prev(uncrossed.end())); // secondSlowestUncrossed
         }
-        solveAux(fast_uncrossed, fast_crossed, totalTime + tFor2Fastest, lowestTotalTime, fast_steps, lowestTotalSteps);
     }
-    { // Send the two slowest
-        auto slow_steps = steps;
-        slow_steps.push_back({secondSlowestUncrossed, slowestUncrossed});
-        auto slow_uncrossed = uncrossed;
-        slow_uncrossed.erase(prev(slow_uncrossed.end()));
-        slow_uncrossed.erase(prev(slow_uncrossed.end()));
-        auto slow_crossed = crossed;
-        slow_crossed.emplace(secondSlowestUncrossed);
-        slow_crossed.emplace(slowestUncrossed);
-        if (!slow_uncrossed.empty()) {
-            slow_steps.push_back({personSentBackIf2SlowestWent});
-            slow_crossed.erase(personSentBackIf2SlowestWent);
-            slow_uncrossed.emplace(personSentBackIf2SlowestWent);
-        } else {
-            tFor2Slowest -= personSentBackIf2SlowestWent;
-        }
-        solveAux(slow_uncrossed, slow_crossed, totalTime + tFor2Slowest, lowestTotalTime, slow_steps, lowestTotalSteps);
+    if (uncrossed.size() == 2) {
+        lowestTotalSteps.push_back({*uncrossed.begin(), *next(uncrossed.begin())});
+        lowestTotalTime += max(*uncrossed.begin(), *next(uncrossed.begin()));
+    } else if (uncrossed.size() == 1) {
+        throw invalid_argument("There should never be a single person left uncrossed.");
     }
 }
 
 void solve(const vector<int> & peopleSpeeds) {
-    if (peopleSpeeds.size() == 1) {
-        cout << *peopleSpeeds.begin() << endl;
-        return;
-    }
-    int lowestTotalTime = INT_MAX;
+    int lowestTotalTime = 0;
     vector<vector<int>> lowestTotalSteps;
-    solveAux(multiset<int>(peopleSpeeds.begin(), peopleSpeeds.end()), {}, 0, lowestTotalTime, {}, lowestTotalSteps);
+    solveAux(multiset<int>(peopleSpeeds.begin(), peopleSpeeds.end()), lowestTotalTime, lowestTotalSteps);
     cout << lowestTotalTime << endl;
     printSteps(lowestTotalSteps);
+    cout << "\n";
 }
 
 int main() {
-    solve({1, 2, 5, 10});
+    ifstream inFile("input.txt");
+    int numInputs;
+    inFile >> numInputs;
+    for (int inputi = 0; inputi < numInputs; ++inputi) {
+        int sizeOfInput;
+        inFile >> sizeOfInput;
+        vector<int> speeds;
+        for (int speedi = 0; speedi < sizeOfInput; ++speedi) {
+            int speed;
+            inFile >> speed;
+            speeds.push_back(speed);
+        }
+        solve(speeds);
+    }
 }
