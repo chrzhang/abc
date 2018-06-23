b9637f307be8973ead2960ddff045fb22940cebb
Christopher Zhang <christopherzhang3@gmail.com>
2018-06-23 16:55:18 -0400

    [TugOfWar] Use DP to remember sum for j items from indices 0 to i and reuse that information for bigger i, sum, and j values.

--- uva/tug_of_war/main.cpp
+++ uva/tug_of_war/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <climits>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,45 +12,56 @@ int sum(const vector<int> & team) {
     for (const auto & t : team) total += t;
     return total;
 }
-
 pair<int, int> solve(const vector<int> & weights) {
-    if (weights.size() == 0) { return make_pair(0, 0); }
-    if (weights.size() == 1) { return make_pair(0, weights[0]); }
-    vector<int> team_A, team_B;
-    auto mid_iter = next(weights.begin(), weights.size() / 2);
-    team_A = vector<int>(weights.begin(), mid_iter);
-    team_B = vector<int>(mid_iter, weights.end());
-    int team_A_sum = sum(team_A);
-    int team_B_sum = sum(team_B);
-    int curr_sum_diff = abs(team_A_sum - team_B_sum);
-    for (auto & team_A_member : team_A) { // Consider swapping each member
-        // Find best candidate in team B to swap with
-        int smallest_pot_diff = curr_sum_diff;
-        int smallest_pot_diff_A_sum = 0;
-        int smallest_pot_diff_B_sum = 0;
-        int * swap_from_a = nullptr;
-        int * swap_from_b = nullptr;
-        for (auto & team_B_member : team_B) {
-            const int pot_A_sum = team_A_sum - team_A_member + team_B_member;
-            const int pot_B_sum = team_B_sum - team_B_member + team_A_member;
-            const int pot_new_diff = abs(pot_A_sum - pot_B_sum);
-            if (pot_new_diff < smallest_pot_diff) {
-                smallest_pot_diff = pot_new_diff;
-                smallest_pot_diff_A_sum = pot_A_sum;
-                smallest_pot_diff_B_sum = pot_B_sum;
-                swap_from_a = &team_A_member;
-                swap_from_b = &team_B_member;
+    const size_t total_weights = accumulate(weights.begin(), weights.end(), 0);
+    // A 3D matrix indexed by [i][sum][j]
+    vector<vector<vector<bool>>> dp(
+        weights.size() + 1, // i, the end index of a range [0..i]
+        vector<vector<bool>>(total_weights + 1, // sum, the total of a subset
+            vector<bool>(weights.size() / 2 + 1, false))); // j, the # of elements in said subset
+    // dp[i][sum][j] == true means we can fit j elements from the range [0..i] to make sum
+    // If f[i-1][sum][j], so is f[i][sum][j]
+    // If f[i][sum][j], f[i+1][sum + weights[i]][j + 1]
+    for (size_t i = 0; i < weights.size() + 1; ++i) {
+        for (size_t sum = 0; sum < total_weights + 1; ++sum) {
+            for (size_t j = 0; j < weights.size() / 2 + 1; ++j) {
+                dp[i][sum][j] = false;
             }
         }
-        if (smallest_pot_diff < curr_sum_diff) {
-            curr_sum_diff = smallest_pot_diff;
-            team_A_sum = smallest_pot_diff_A_sum;
-            team_B_sum = smallest_pot_diff_B_sum;
-            swap(*swap_from_a, *swap_from_b);
+    }
+    for (size_t i = 0; i < weights.size() + 1; ++i) {
+        for (size_t sum = 0; sum < total_weights + 1; ++sum) {
+            for (size_t j = 0; j < weights.size() / 2 + 1; ++j) {
+                if (sum == 0 && j == 0) {
+                    dp[i][sum][j] = true;
+                }
+                if (i > 0 && dp[i - 1][sum][j]) {
+                    dp[i][sum][j] = true;
+                }
+                if (dp[i][sum][j] && i + 1 < weights.size() + 1) {
+                    dp[i + 1][sum + weights[i]][j + 1] = true;
+                }
+            }
         }
     }
-    return make_pair(min(team_A_sum, team_B_sum),
-                     max(team_A_sum, team_B_sum));
+
+    int best_diff = INT_MAX;
+    int best_diff_a = 0;
+    int best_diff_b = 0;
+    // Look along j = weights.size() / 2
+    for (size_t i = 0; i < weights.size() + 1; ++i) {
+        for (size_t sum = 0; sum < total_weights + 1; ++sum) {
+            if (dp[i][sum][weights.size() / 2]) {
+                const size_t other_sum = total_weights - sum;
+                if (abs((int) sum - (int) other_sum) < best_diff) {
+                    best_diff = abs((int) sum - (int) other_sum);
+                    best_diff_a = min(sum, other_sum);
+                    best_diff_b = max(sum, other_sum);
+                }
+            }
+        }
+    }
+    return make_pair(best_diff_a, best_diff_b);
 }
 
 void print_result(const pair<int, int> & result) {
@@ -57,13 +69,15 @@ void print_result(const pair<int, int> & result) {
 }
 
 int main(int argc, const char * argv[]) {
+    assert(make_pair(20, 68) == solve(vector<int>({68, 20})));
+    assert(make_pair(100, 100) == solve(vector<int>({68, 50, 32, 30, 20})));
+    assert(make_pair(190, 200) == solve(vector<int>({100, 90, 200})));
+    assert(make_pair(243, 243) == solve(vector<int>({120, 111, 111, 101, 20, 11, 11, 1})));
+    assert(make_pair(1879, 1880) == solve(vector<int>({142, 292, 446, 124, 171, 32, 430, 237, 4, 398, 217, 261, 354, 263, 59, 329})));
     if (2 != argc) {
         cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
-    assert(make_pair(20, 68) == solve(vector<int>({68, 20})));
-    assert(make_pair(100, 100) == solve(vector<int>({68, 50, 32, 30, 20})));
-    assert(make_pair(190, 200) == solve(vector<int>({100, 90, 200})));
     ifstream inFile(argv[1]);
     int input_ct;
     inFile >> input_ct;
@@ -77,6 +91,7 @@ int main(int argc, const char * argv[]) {
             weights.push_back(weight);
         }
         print_result(solve(weights));
+        cout << endl;
     }
     return 0;
 }
