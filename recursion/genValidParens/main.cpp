dda665ed69f0d73388d44a266c1387ad779cb329
Christopher Zhang <christopherzhang3@gmail.com>
2018-10-05 00:52:10 -0400

    Retab.

--- recursion/genValidParens/main.cpp
+++ recursion/genValidParens/main.cpp
@@ -13,24 +13,24 @@ using namespace std;
 // nc - number of closing )s left
 void gaux(const int no, const int nc, const string & s,
           vector<string> & result) {
-	if (no + nc == 0) {
-		result.push_back(s);
-		return;
-	}
-	const int bal = nc - no; // Since no - nc == bal
-	assert(bal >= 0);
-	if (nc && bal > 0) {
-		gaux(no, nc - 1, s + ")", result);
-	}
-	if (no) {
-		gaux(no - 1, nc, s + "(", result);
-	}
+    if (no + nc == 0) {
+        result.push_back(s);
+        return;
+    }
+    const int bal = nc - no; // Since no - nc == bal
+    assert(bal >= 0);
+    if (nc && bal > 0) {
+        gaux(no, nc - 1, s + ")", result);
+    }
+    if (no) {
+        gaux(no - 1, nc, s + "(", result);
+    }
 }
 
 vector<string> generateParenthesis(int n) {
-	vector<string> result;
-	gaux(n, n, "", result);
-	return result;
+    vector<string> result;
+    gaux(n, n, "", result);
+    return result;
 }
 
 int main() {
