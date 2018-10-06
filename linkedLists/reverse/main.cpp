a24fe2af5ae3a65e1cce0b2823a1cd4d90a9406d
Christopher Zhang <christopherzhang3@gmail.com>
2018-10-06 00:57:11 -0400

    Reverse k-wide groups in a list.

--- linkedLists/reverse/main.cpp
+++ linkedLists/reverse/main.cpp
@@ -1,122 +1,89 @@
 #include <iostream>
 #include <cassert>
 
-#define N 10
+using namespace std;
+// Reverse a linked list in k-groups
 
-// Reverse a linked list
-
-struct Node {
+struct ListNode {
     int val;
-    Node * next;
-    Node(int v) : val(v), next(nullptr) {}
+    ListNode * next;
+    ListNode(const int v) : val(v), next(nullptr) {}
 };
 
-void swap(Node * & nptr1, Node * & nptr2) {
-    auto temp = nptr1;
-    nptr1 = nptr2;
-    nptr2 = temp;
+ListNode * raux(ListNode * h, ListNode * e) {
+    if (h == e) {
+        h->next = nullptr;
+        return h;
+    }
+    ListNode * f = raux(h->next, e);
+    f->next = h;
+    return h;
 }
 
-void advancePtr(Node * & p, unsigned n) {
-    for (unsigned i = 0; i < n; ++i) {
-         if (!p) { return; }
-         p = p->next;
-    }
+ListNode * reverse(ListNode * h, ListNode * e) {
+    ListNode * r = raux(h, e);
+    r->next = nullptr;
+    return e;
 }
 
-struct List {
-    Node * head, * tail;
-    List() : head(nullptr), tail(nullptr) {}
-    ~List() {
-        auto curr = head;
-        while (curr) {
-            auto temp = curr->next;
-            delete curr;
-            curr = temp;
-        }
+void revk(ListNode * t, const int k) {
+    assert(t);
+    ListNode * h = t->next;
+    ListNode * e = h;
+    for (int i = 0; i < k - 1; ++i) {
+        if (!e) { return; }
+        e = e->next;
     }
-    void add(int val) {
-        auto n = new Node(val);
-        if (!head) {
-            head = tail = n;
-        } else {
-            auto oldTail = tail;
-            tail = n;
-            oldTail->next = tail;
-        }
+    if (!e) { return; }
+    t->next = e;
+    ListNode * future_h = e->next;
+    reverse(h, e);
+    h->next = future_h;
+    revk(h, k);
+}
+
+ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode sentinel(0);
+    sentinel.next = head;
+    revk(&sentinel, k);
+    return sentinel.next;
+}
+
+void print_list(const ListNode * head) {
+    const ListNode * curr = head;
+    while (curr) {
+        cout << curr->val << " ";
+        curr = curr->next;
     }
-    void print() const {
-        auto curr = head;
-        while (curr) {
-            std::cout << curr->val << " ";
-            curr = curr->next;
-        }
-        std::cout << std::endl;
-    }
-    void reverse() {
-        auto curr = head;
-        Node * prev = nullptr;
-        while (curr) {
-            auto temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        swap(head, tail);
-    }
-    void reverseGroupAux(Node * pred, Node * head, Node * tail, Node * succ) {
-        if (pred) { assert(pred->next == head); }
-        assert(tail->next == succ);
-        tail->next = nullptr; // Isolate list
-        List l;
-        l.head = head;
-        l.tail = tail;
-        l.reverse();
-        if (pred) { pred->next = l.head; }
-        l.tail->next = succ;
-        l.head = l.tail = nullptr; // Don't destroy nodes
-    }
-    void reverseGroup(unsigned n) {
-        if (n < 2) { return; }
-        Node * p, * h, * t, * s;
-        Node * np, * nh, * nt, * ns;
-        p = nullptr;
-        h = head;
-        t = head; advancePtr(t, n - 1);
-        s = nullptr;
-        if (t) {
-            s = t->next;
-            head = t;
-        }
-        while (h && t) {
-            np = h;
-            nh = t->next;
-            nt = nh; advancePtr(nt, n - 1);
-            if (nt) { ns = nt->next; } else { ns = nullptr; }
-            // DO WORK
-            if (!t->next) {
-                tail = h;
-            }
-            reverseGroupAux(p, h, t, s);
-            p = np; h = nh; t = nt; s = ns;
-        }
-    }
-};
+    cout << endl;
+}
 
 int main() {
-    for (int n = 0; n < N; ++n) {
-        std::cout << "n = " << n << std::endl;
-        for (int k = 1; k < 4; ++k) {
-            std::cout << "\tk = " << k << std::endl;
-            List l;
-            for (int i = 0; i < n; ++i) {
-                l.add(i);
-            }
-            l.print();
-            l.reverseGroup(k);
-            l.print();
-            std::cout << "\n";
-        }
+    {
+        ListNode l1(1);
+        ListNode l2(2);
+        ListNode l3(3);
+        ListNode l4(4);
+        ListNode l5(5);
+        l1.next = &l2;
+        l2.next = &l3;
+        l3.next = &l4;
+        l4.next = &l5;
+        ListNode * result = reverseKGroup(&l1, 2);
+        print_list(result);
+    }
+    {
+        ListNode l1(1);
+        ListNode l2(2);
+        ListNode l3(3);
+        ListNode l4(4);
+        ListNode l5(5);
+        l1.next = &l2;
+        l2.next = &l3;
+        l3.next = &l4;
+        l4.next = &l5;
+        ListNode * result = reverseKGroup(&l1, 3);
+        print_list(result);
     }
     return 0;
 }
