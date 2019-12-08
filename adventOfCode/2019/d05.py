18dcff46713a21620dd5fd0f4ba8bdf4dbefdab2
Christopher Zhang <christopherzhang3@gmail.com>
2019-12-08 12:25:04 -0500

    [AOC19] Finish Day 5.

--- adventOfCode/2019/d05.py
+++ adventOfCode/2019/d05.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import functools
 from enum import Enum, unique
 
 with open("inputs/day5_input", "r") as f:
@@ -14,6 +15,10 @@ class OpCode(Enum):
     MULTIPLY = "02"
     INPUT = "03"
     OUTPUT = "04"
+    JUMP_IF_TRUE = "05"
+    JUMP_IF_FALSE = "06"
+    LESS_THAN = "07"
+    EQUALS = "08"
     HALT = "99"
 
 
@@ -34,10 +39,12 @@ class InputMoreThanOnceError(Exception):
 
 
 def number_of_values(opcode):
-    if opcode in (OpCode.ADD, OpCode.MULTIPLY):
+    if opcode in (OpCode.ADD, OpCode.MULTIPLY, OpCode.LESS_THAN, OpCode.EQUALS):
         return 4
     if opcode in (OpCode.INPUT, OpCode.OUTPUT):
         return 2
+    if opcode in (OpCode.JUMP_IF_FALSE, OpCode.JUMP_IF_TRUE):
+        return 3
     raise UnknownOpCodeError
 
 
@@ -94,8 +101,7 @@ def enact_multiply(modes, states, idx):
     states[pos_of_result] = param_1 * param_2
 
 
-def enact_input(modes, states, idx):
-    input_value = 1
+def enact_input(modes, states, idx, input_value):
     if modes.first() != Mode.POSITION:
         raise WritingToImmediateError
     pos_of_result = states[idx + 1]
@@ -112,20 +118,54 @@ def enact_output(modes, states, idx, outputs):
         raise UnknownModeError
 
 
-def enact_opcode(opcode, modes, states, idx, outputs):
+def enact_jump_if_condition(condition, modes, states, idx):
+    param_1 = read_param(modes.first(), states[idx + 1], states)
+    if condition(param_1):
+        return read_param(modes.second(), states[idx + 2], states)
+    return None
+
+
+enact_jump_if_true = functools.partial(enact_jump_if_condition, lambda p: p)
+enact_jump_if_false = functools.partial(enact_jump_if_condition, lambda p: not p)
+
+
+def enact_less_than(modes, states, idx):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx)
+    states[pos_of_result] = 1 if param_1 < param_2 else 0
+
+
+def enact_equals(modes, states, idx):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx)
+    states[pos_of_result] = 1 if param_1 == param_2 else 0
+
+
+def enact_opcode(opcode, modes, states, idx, outputs, input_value):
+    idx_target = None
     if opcode is OpCode.ADD:
         enact_add(modes, states, idx)
     elif opcode is OpCode.MULTIPLY:
         enact_multiply(modes, states, idx)
     elif opcode is OpCode.INPUT:
-        enact_input(modes, states, idx)
+        enact_input(modes, states, idx, input_value)
     elif opcode is OpCode.OUTPUT:
         enact_output(modes, states, idx, outputs)
+    elif opcode is OpCode.JUMP_IF_TRUE:
+        idx_target = enact_jump_if_true(modes, states, idx)
+    elif opcode is OpCode.JUMP_IF_FALSE:
+        idx_target = enact_jump_if_false(modes, states, idx)
+    elif opcode is OpCode.LESS_THAN:
+        enact_less_than(modes, states, idx)
+    elif opcode is OpCode.EQUALS:
+        enact_equals(modes, states, idx)
     else:
         raise UnknownOpCodeError
+    if idx_target is None:
+        return idx + number_of_values(opcode)
+    else:
+        return idx_target
 
 
-def solve(states):
+def solve(input_val, states):
     states = list(states)
     outputs = []
     i = 0
@@ -138,14 +178,21 @@ def solve(states):
             if input_used:
                 raise InputMoreThanOnceError
             input_used = True
-        enact_opcode(opcode, modes, states, i, outputs)
-        i += number_of_values(opcode)
+        i = enact_opcode(opcode, modes, states, i, outputs, input_val)
     return states, outputs
 
 
-assert [1002, 4, 3, 4, 99], [] == solve([1002, 4, 3, 4, 33])
-assert [3, 0, 4, 0, 99], [1] == solve([3, 0, 4, 0, 99])
-assert [1101, 100, -1, 4, 99], [] == solve([1101, 100, -1, 4, 0])
+solve_part_1 = functools.partial(solve, 1)
 
-_, outputs = solve(read_states)
+
+assert [1002, 4, 3, 4, 99], [] == solve_part_1([1002, 4, 3, 4, 33])
+assert [3, 0, 4, 0, 99], [1] == solve_part_1([3, 0, 4, 0, 99])
+assert [1101, 100, -1, 4, 99], [] == solve_part_1([1101, 100, -1, 4, 0])
+
+_, outputs = solve_part_1(read_states)
 assert outputs[-1] == 6761139
+
+
+solve_part_2 = functools.partial(solve, 5)
+_, outputs = solve_part_2(read_states)
+assert outputs == [9217546]
