787270c2b5b085b1bba08fcc3a50979445c13daf
Christopher Zhang <christopherzhang3@gmail.com>
2019-12-09 21:55:38 -0500

    [AOC19] Finish Day 9.

---
+++ adventOfCode/2019/d09.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python3
+
+import functools
+from collections import defaultdict
+from enum import Enum, unique
+
+@unique
+class OpCode(Enum):
+    ADD = "01"
+    MULTIPLY = "02"
+    INPUT = "03"
+    OUTPUT = "04"
+    JUMP_IF_TRUE = "05"
+    JUMP_IF_FALSE = "06"
+    LESS_THAN = "07"
+    EQUALS = "08"
+    ADJUST_REL_BASE = "09"
+    HALT = "99"
+
+
+class UnknownOpCodeError(Exception):
+    pass
+
+
+class UnknownModeError(Exception):
+    pass
+
+
+class WritingToImmediateError(Exception):
+    pass
+
+
+class InputMoreThanOnceError(Exception):
+    pass
+
+
+def number_of_values(opcode):
+    if opcode in (OpCode.ADD, OpCode.MULTIPLY, OpCode.LESS_THAN, OpCode.EQUALS):
+        return 4
+    if opcode in (OpCode.INPUT, OpCode.OUTPUT, OpCode.ADJUST_REL_BASE):
+        return 2
+    if opcode in (OpCode.JUMP_IF_FALSE, OpCode.JUMP_IF_TRUE):
+        return 3
+    raise UnknownOpCodeError
+
+
+@unique
+class Mode(Enum):
+    POSITION = "0"
+    IMMEDIATE = "1"
+    RELATIVE = "2"
+
+
+class Modes:
+    def __init__(self, modes):
+        self._modes = [Mode(m) for m in modes]
+
+    def first(self):
+        return self._modes[2]
+
+    def second(self):
+        return self._modes[1]
+
+    def third(self):
+        return self._modes[0]
+
+
+def parse_opcode_and_modes(first_value):
+    padded = str(first_value).zfill(5)
+    opcode = OpCode(padded[-2:])
+    modes = Modes(padded[:3])
+    return opcode, modes
+
+class RelativeBase:
+    def __init__(self, val):
+        self._val = val
+
+    def set_val(self, val):
+        self._val = val
+
+    def val(self):
+        return self._val
+
+def read_param(mode, param, states, relative_base):
+    if mode == Mode.IMMEDIATE:
+        return param
+    if mode == Mode.POSITION:
+        return states[param]
+    if mode == Mode.RELATIVE:
+        return states[param + relative_base.val()]
+
+
+def get_params(modes, states, idx, relative_base):
+    param_1 = read_param(modes.first(), states[idx + 1], states, relative_base)
+    param_2 = read_param(modes.second(), states[idx + 2], states, relative_base)
+    if modes.third() is Mode.IMMEDIATE:
+        raise WritingToImmediateError
+    if modes.third() is Mode.POSITION:
+        pos_of_result = states[idx + 3]
+    elif modes.third() is Mode.RELATIVE:
+        pos_of_result = relative_base.val() + states[idx + 3]
+    return param_1, param_2, pos_of_result
+
+
+def enact_add(modes, states, idx, relative_base):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx, relative_base)
+    states[pos_of_result] = param_1 + param_2
+
+
+def enact_multiply(modes, states, idx, relative_base):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx, relative_base)
+    states[pos_of_result] = param_1 * param_2
+
+
+def enact_input(modes, states, idx, input_value, relative_base):
+    if modes.first() == Mode.IMMEDIATE:
+        raise WritingToImmediateError
+    if modes.first() == Mode.POSITION:
+        pos_of_result = states[idx + 1]
+        states[pos_of_result] = input_value
+    elif modes.first() == Mode.RELATIVE:
+        pos_of_result = relative_base.val() + states[idx + 1]
+        states[pos_of_result] = input_value
+
+
+def enact_output(modes, states, idx, outputs, relative_base):
+    if modes.first() is Mode.POSITION:
+        pos_of_result = states[idx + 1]
+        outputs.append(states[pos_of_result])
+    elif modes.first() is Mode.IMMEDIATE:
+        outputs.append(states[idx + 1])
+    elif modes.first() is Mode.RELATIVE:
+        pos_of_result = relative_base.val() + states[idx + 1]
+        outputs.append(states[pos_of_result])
+    else:
+        raise UnknownModeError
+
+
+def enact_jump_if_condition(condition, modes, states, idx, relative_base):
+    param_1 = read_param(modes.first(), states[idx + 1], states, relative_base)
+    if condition(param_1):
+        return read_param(modes.second(), states[idx + 2], states, relative_base)
+    return None
+
+
+enact_jump_if_true = functools.partial(enact_jump_if_condition, lambda p: p)
+enact_jump_if_false = functools.partial(enact_jump_if_condition, lambda p: not p)
+
+
+def enact_less_than(modes, states, idx, relative_base):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx, relative_base)
+    states[pos_of_result] = 1 if param_1 < param_2 else 0
+
+
+def enact_equals(modes, states, idx, relative_base):
+    param_1, param_2, pos_of_result = get_params(modes, states, idx, relative_base)
+    states[pos_of_result] = 1 if param_1 == param_2 else 0
+
+
+def enact_adjust_relative_base(modes, states, idx, relative_base):
+    param_1 = read_param(modes.first(), states[idx + 1], states, relative_base)
+    relative_base.set_val(param_1 + relative_base.val())
+
+def enact_opcode(opcode, modes, states, idx, relative_base, outputs, input_value):
+    idx_target = None
+    if opcode is OpCode.ADD:
+        enact_add(modes, states, idx, relative_base)
+    elif opcode is OpCode.MULTIPLY:
+        enact_multiply(modes, states, idx, relative_base)
+    elif opcode is OpCode.INPUT:
+        enact_input(modes, states, idx, input_value, relative_base)
+    elif opcode is OpCode.OUTPUT:
+        enact_output(modes, states, idx, outputs, relative_base)
+    elif opcode is OpCode.JUMP_IF_TRUE:
+        idx_target = enact_jump_if_true(modes, states, idx, relative_base)
+    elif opcode is OpCode.JUMP_IF_FALSE:
+        idx_target = enact_jump_if_false(modes, states, idx, relative_base)
+    elif opcode is OpCode.LESS_THAN:
+        enact_less_than(modes, states, idx, relative_base)
+    elif opcode is OpCode.EQUALS:
+        enact_equals(modes, states, idx, relative_base)
+    elif opcode is OpCode.ADJUST_REL_BASE:
+        enact_adjust_relative_base(modes, states, idx, relative_base)
+    else:
+        raise UnknownOpCodeError
+    if idx_target is None:
+        return idx + number_of_values(opcode)
+    else:
+        return idx_target
+
+
+def solve(input_val, read_states):
+    states = defaultdict(int)
+    for idx, val in enumerate(read_states):
+        states[idx] = val
+    relative_base = RelativeBase(0)
+    outputs = []
+    i = 0
+    input_used = False
+    while True:
+        opcode, modes = parse_opcode_and_modes(states[i])
+        if opcode is OpCode.HALT:
+            break
+        if opcode is OpCode.INPUT:
+            if input_used:
+                raise InputMoreThanOnceError
+            input_used = True
+        i = enact_opcode(opcode, modes, states, i, relative_base, outputs, input_val)
+    return states, outputs
+
+
+if __name__ == "__main__":
+    with open("inputs/day9_input", "r") as f:
+        (line,) = f.read().strip().split("\n")
+    read_states = tuple([int(x) for x in line.split(",")])
+    _, (output,) = solve(1, read_states)
+    assert 2682107844 == output
+    _, (output,) = solve(2, read_states)
+    assert 34738 == output
