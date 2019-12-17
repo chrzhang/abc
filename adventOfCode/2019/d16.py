c7257bb2df2c7d0acfe64f970add7fa27d8a5734
Christopher Zhang <christopherzhang3@gmail.com>
2019-12-16 20:27:33 -0500

    [AOC19] Day 16 Part 2.

--- adventOfCode/2019/d16.py
+++ adventOfCode/2019/d16.py
@@ -25,11 +25,39 @@ def fft(digits):
     return new_digits
 
 
+def fft_second_half(second_half):
+    new_half = []
+    total_sum = sum(second_half)
+    for idx, _ in enumerate(second_half):
+        new_half.append(total_sum % 10)
+        total_sum -= second_half[idx]
+    return new_half
+
+
 def part_1(line, phase_ct):
     digits = [int(d) for d in line]
     for _ in range(phase_ct):
         digits = fft(digits)
-    return "".join(str(d) for d in digits)
+    return "".join([str(d) for d in digits][:8])
+
+
+def get_offset(line):
+    return int(line[:7])
+
+
+def get_message_at_offset(line, offset):
+    return line[offset : offset + 8]
+
+
+def part_2(line, phase_ct):
+    offset = get_offset(line)
+    digits = [int(d) for d in line * 10000]
+    second_half = digits[len(digits) // 2 :]
+    for _ in range(phase_ct):
+        second_half = fft_second_half(second_half)
+    offset_in_second_half = offset - len(digits) // 2
+    second_half_str = "".join([str(d) for d in second_half])
+    return get_message_at_offset(second_half_str, offset_in_second_half)
 
 
 def run_tests():
@@ -37,13 +65,20 @@ def run_tests():
     assert part_1("12345678", 2) == "34040438"
     assert part_1("12345678", 3) == "03415518"
     assert part_1("12345678", 4) == "01029498"
-    assert part_1("80871224585914546619083218645595", 100)[:8] == "24176176"
-    assert part_1("19617804207202209144916044189917", 100)[:8] == "73745418"
-    assert part_1("69317163492948606335995924319873", 100)[:8] == "52432133"
+    assert part_1("80871224585914546619083218645595", 100) == "24176176"
+    assert part_1("19617804207202209144916044189917", 100) == "73745418"
+    assert part_1("69317163492948606335995924319873", 100) == "52432133"
+    assert get_offset("123456789") == 1234567
+    assert get_message_at_offset("98765432109876543210", 7) == "21098765"
+    assert [6, 1, 5, 8] == fft_second_half([5, 6, 7, 8])
+    assert part_2("03036732577212944063491565474664", 100) == "84462026"
+    assert part_2("02935109699940807407585447034323", 100) == "78725270"
+    assert part_2("03081770884921959731165446850517", 100) == "53553731"
 
 
 if __name__ == "__main__":
     run_tests()
     with open("inputs/day16_input", "r") as f:
         (line,) = f.read().strip().split("\n")
-    assert "69549155" == part_1(line, 100)[:8]
+    assert "69549155" == part_1(line, 100)
+    assert "83253465" == part_2(line, 100)
